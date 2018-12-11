2795fe9213835ebb78affc4995564844d1df6e2b
gouarin <loic.gouarin@gmail.com>
2018-12-11 15:48:25 +0100

    fix interval_op.cpp

--- demos/interval_op.cpp
+++ demos/interval_op.cpp
@@ -1,4 +1,5 @@
-#include <mure/intervals_op.hpp>
+#include <mure/subset.hpp>
+#include <mure/intervals_operator.hpp>
 #include <mure/level_cell_array.hpp>
 #include <mure/level_cell_list.hpp>
 #include <mure/mr_config.hpp>
@@ -9,18 +10,76 @@ int main()
     using config = mure::MRConfig<dim>;
 
     mure::LevelCellList<config> level_cell_list_1;
-    level_cell_list_1[{}].add_interval({0, 2});
-    level_cell_list_1[{}].add_interval({4, 7});
+    level_cell_list_1[{}].add_interval({0, 3});
+    // level_cell_list_1[{}].add_interval({4, 7});
     mure::LevelCellArray<config> level_cell_array_1{level_cell_list_1};
 
     mure::LevelCellList<config> level_cell_list_2;
-    level_cell_list_2[{}].add_interval({1, 5});
+    level_cell_list_2[{}].add_interval({0, 2});
+    // level_cell_list_2[{}].add_interval({1, 5});
     mure::LevelCellArray<config> level_cell_array_2{level_cell_list_2};
 
-    std::cout << "intersection\n";
-    mure::intersection(level_cell_array_1, level_cell_array_2);
-    std::cout << "union\n";
-    mure::union_(level_cell_array_1, level_cell_array_2);
-    std::cout << "difference\n";
-    mure::difference(level_cell_array_1, level_cell_array_2);
+    mure::LevelCellList<config> level_cell_list_3;
+    level_cell_list_3[{}].add_interval({3, 10});
+    mure::LevelCellArray<config> level_cell_array_3{level_cell_list_3};
+
+    // auto expr = mure::union_(mure::_1, mure::_2, mure::_3);
+    auto expr = mure::intersection(mure::_1, mure::_2);
+    auto set = mure::make_subset<config>(expr, level_cell_array_1, level_cell_array_2);
+    set.apply([](auto& index_yz, auto& interval){std::cout << index_yz << " " << interval << "\n";});
+
+
+    /////////////////////////////////////////
+    //
+    // 2D
+    //
+    /////////////////////////////////////////
+    // constexpr std::size_t dim = 2;
+    // using config = mure::MRConfig<dim>;
+
+    // mure::LevelCellList<config> level_cell_list_1;
+    // level_cell_list_1.extend({0}, {4});
+    // level_cell_list_1[0].add_interval({0, 2});
+    // level_cell_list_1[0].add_interval({4, 7});
+    // mure::LevelCellArray<config> level_cell_array_1{level_cell_list_1};
+
+    // mure::LevelCellList<config> level_cell_list_2;
+    // level_cell_list_2.extend({0}, {4});
+    // level_cell_list_2[0].add_interval({0, 2});
+    // level_cell_list_2[1].add_interval({0, 2});
+
+    // mure::LevelCellArray<config> level_cell_array_2{level_cell_list_2};
+
+
+    // auto expr = mure::difference(mure::_1, mure::_2);
+    // auto set = mure::make_subset<config>(expr, level_cell_array_1, level_cell_array_2);
+    // set.apply([](auto& index_yz, auto& interval){std::cout << index_yz << " " << interval << "\n";});
+
+    /////////////////////////////////////////
+    //
+    // 3D
+    //
+    /////////////////////////////////////////
+    // constexpr std::size_t dim = 3;
+    // using config = mure::MRConfig<dim>;
+
+    // mure::LevelCellList<config> level_cell_list_1;
+    // level_cell_list_1.extend({0, 0}, {4, 4});
+    // level_cell_list_1[{0, 0}].add_interval({0, 2});
+    // // level_cell_list_1[0].add_interval({4, 7});
+    // mure::LevelCellArray<config> level_cell_array_1{level_cell_list_1};
+
+    // mure::LevelCellList<config> level_cell_list_2;
+    // level_cell_list_2.extend({0, 0}, {4, 4});
+    // level_cell_list_2[{0, 0}].add_interval({0, 5});
+    // level_cell_list_2[{0, 1}].add_interval({0, 2});
+
+    // mure::LevelCellArray<config> level_cell_array_2{level_cell_list_2};
+
+    // auto expr = mure::intersection(mure::_2, mure::_1);
+    // auto level_cell_arrays = std::tie(level_cell_array_1,
+    //                                   level_cell_array_2);
+
+    // auto set = mure::make_subset<config>(expr, level_cell_array_1, level_cell_array_2);
+    // set.apply([](auto& index_yz, auto& interval){std::cout << index_yz << " " << interval << "\n";});
 }
