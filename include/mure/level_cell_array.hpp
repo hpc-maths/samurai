2caa33925bf80339c93de00430f3dcc07e95c65d
Roland Denis <roland.denis@math.univ-lyon1.fr>
2018-12-16 12:21:57 +0100

    Fixing indexing issue in level_cell_array

--- include/mure/level_cell_array.hpp
+++ include/mure/level_cell_array.hpp
@@ -203,14 +203,13 @@ namespace mure
         for(index_t i = beg_index; i < end_index; ++i)
         {
             coord_index_t c;
-            index_t ic;
-            for(c = m_cells[i].start, ic=0; c < m_cells[i].end; ++c, ++ic)
+            for(c = m_cells[i].start; c < m_cells[i].end; ++c)
             {
                 index_yz[d - 1] = c;
                 for_each_interval_in_x_impl(std::forward<function_t>(f),
                                             index_yz,
-                                            m_offsets[m_cells[i].index + ic],
-                                            m_offsets[m_cells[i].index + ic + 1],
+                                            m_offsets[m_cells[i].index + c],
+                                            m_offsets[m_cells[i].index + c + 1],
                                             std::integral_constant<index_t, d-1>{});
             }
         }
