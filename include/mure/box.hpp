f4a6b07c4a9e94758416f789fced17c16b5fed48
gouarin <loic.gouarin@gmail.com>
2018-12-19 23:07:40 +0100

    add Box methods

--- include/mure/box.hpp
+++ include/mure/box.hpp
@@ -19,6 +19,13 @@ namespace mure
             m_max_corner.fill(0);
         }
 
+        template<typename T>
+        inline Box(Box<T, dim_> const& box)
+        {
+            m_min_corner = box.min_corner();
+            m_max_corner = box.max_corner();
+        }
+
         inline Box(point_t const& min_corner, point_t const& max_corner):
             m_min_corner{min_corner}, m_max_corner{max_corner}{}
 
@@ -42,9 +49,19 @@ namespace mure
             return m_max_corner;
         }
 
+        inline point_t length() const
+        {
+            return (m_max_corner - m_min_corner);
+        }
+
         inline bool is_valid() const
         {
             return xt::all(m_min_corner < m_max_corner);
+        }
+
+        inline Box operator*(value_t v) const
+        {
+            return {v*m_min_corner, v*m_max_corner};
         }
 
     private:
@@ -52,6 +69,20 @@ namespace mure
         point_t m_min_corner;
         point_t m_max_corner;
     };
+
+    template<typename value_t, std::size_t dim_>
+    inline auto operator*(Box<value_t, dim_> const& box, value_t v)
+    {
+        Box<value_t, dim_> that(box);
+        return that *=v;
+    }
+
+    template<typename value_t, std::size_t dim_>
+    inline auto operator*(value_t v, Box<value_t, dim_> const& box)
+    {
+        Box<value_t, dim_> that(box);
+        return that *=v;
+    }
 
     template<typename value_t, std::size_t dim>
     std::ostream& operator<<(std::ostream& out, Box<value_t, dim> const& box)
