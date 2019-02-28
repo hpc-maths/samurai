6f827f6900e7ad4f89ca1666ef0a11b7a74d890a
gouarin <loic.gouarin@gmail.com>
2019-02-28 13:48:03 +0100

    add step attribute in interval and operators

--- include/mure/interval.hpp
+++ include/mure/interval.hpp
@@ -9,7 +9,7 @@ namespace mure
      * @tparam TValue   Coordinate type (must be signed)
      * @tparam TIndex   Index type
      */
-    template<typename TValue, typename TIndex = std::size_t>
+    template<typename TValue, typename TIndex = int>
     struct Interval
     {
         static_assert(std::is_signed<TValue>::value, "Coordinate type must be signed");
@@ -19,6 +19,7 @@ namespace mure
 
         value_t start = 0;  ///< Interval start
         value_t end   = 0;  ///< Interval end + 1
+        value_t step  = 1;
         index_t index = 0;  ///< Storage index where start the interval's content.
 
         Interval() = default;
@@ -40,12 +41,82 @@ namespace mure
         {
             return (start < end);
         }
+
+        Interval<value_t, index_t>& operator*=(std::size_t i)
+        {
+            start *= i;
+            end *= i;
+            step *= i;
+            return *this;
+        }
+
+        Interval<value_t, index_t>& operator+=(std::size_t i)
+        {
+            start += i;
+            end += i;
+            return *this;
+        }
+
+        Interval<value_t, index_t>& operator-=(std::size_t i)
+        {
+            start -= i;
+            end -= i;
+            return *this;
+        }
     };
 
     template<typename value_t, typename index_t>
     std::ostream& operator<<(std::ostream& out, const Interval<value_t, index_t>& interval)
     {
-        out << "[" << interval.start << "," << interval.end << "[@" << interval.index;
+        out << "[" << interval.start << "," << interval.end << "[@" << interval.index << ":" << interval.step;
         return out;
+    }
+
+    template<typename value_t, typename index_t>
+    Interval<value_t, index_t> operator*(std::size_t i, const Interval<value_t, index_t>& interval)
+    {
+        auto that{interval};
+        that *= i;
+        return that;
+    }
+
+    template<typename value_t, typename index_t>
+    Interval<value_t, index_t> operator*(const Interval<value_t, index_t>& interval, std::size_t i)
+    {
+        auto that{interval};
+        that *= i;
+        return that;
+    }
+
+    template<typename value_t, typename index_t>
+    Interval<value_t, index_t> operator+(std::size_t i, const Interval<value_t, index_t>& interval)
+    {
+        auto that{interval};
+        that += i;
+        return that;
+    }
+
+    template<typename value_t, typename index_t>
+    Interval<value_t, index_t> operator+(const Interval<value_t, index_t>& interval, std::size_t i)
+    {
+        auto that{interval};
+        that += i;
+        return that;
+    }
+
+    template<typename value_t, typename index_t>
+    Interval<value_t, index_t> operator-(std::size_t i, const Interval<value_t, index_t>& interval)
+    {
+        auto that{interval};
+        that -= i;
+        return that;
+    }
+
+    template<typename value_t, typename index_t>
+    Interval<value_t, index_t> operator-(const Interval<value_t, index_t>& interval, std::size_t i)
+    {
+        auto that{interval};
+        that -= i;
+        return that;
     }
 }
