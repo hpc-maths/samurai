c71b097ec598d6f91033f07b92b82618110f01b9
gouarin <loic.gouarin@gmail.com>
2019-09-06 10:51:19 +0200

    add operators_base.hpp

---
+++ include/mure/operators_base.hpp
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <xtensor/xfixed.hpp>
+
+#include "field_expression.hpp"
+#include "utils.hpp"
+
+namespace mure
+{
+    template<template<class T> class OP, class... CT>
+    class field_operator_function
+        : public field_expression<field_operator_function<OP, CT...>> {
+      public:
+        static constexpr std::size_t dim = detail::compute_dim<CT...>();
+
+        field_operator_function(CT &&... e) : m_e{std::forward<CT>(e)...}
+        {}
+
+        template<class interval_t, class... index_t>
+        auto operator()(std::size_t level, interval_t i, index_t... index) const
+        {
+            OP<interval_t> op(level, i, index...);
+            return apply(op);
+        }
+
+        template<class interval_t, class coord_index_t>
+        auto operator()(
+            std::size_t level, interval_t i,
+            xt::xtensor_fixed<coord_index_t, xt::xshape<dim>> index) const
+        {
+            OP<interval_t> op(level, i, index);
+            return apply(op);
+        }
+
+      private:
+        template<class interval_t>
+        auto apply(OP<interval_t> &op) const
+        {
+            return apply_impl(std::make_index_sequence<sizeof...(CT)>(), op);
+        }
+
+        template<std::size_t... I, class interval_t>
+        auto apply_impl(std::index_sequence<I...>, OP<interval_t> &op) const
+        {
+            return op(std::integral_constant<std::size_t, dim>{},
+                      std::get<I>(m_e)...);
+        }
+
+        std::tuple<CT...> m_e;
+    };
+
+    template<template<class T> class OP, class... CT>
+    auto make_field_operator_function(CT &&... e)
+    {
+        return field_operator_function<OP, CT...>(std::forward<CT>(e)...);
+    }
+
+    template<class TInterval>
+    class field_operator_base {
+      public:
+        using interval_t = TInterval;
+        using coord_index_t = typename interval_t::coord_index_t;
+
+        std::size_t level;
+        interval_t i;
+        coord_index_t j, k;
+        double dx;
+
+      protected:
+        template<std::size_t dim>
+        field_operator_base(
+            std::size_t level, interval_t interval,
+            xt::xtensor_fixed<coord_index_t, xt::xshape<dim>> index)
+            : level{level}, i{interval}, dx{1. / (1 << level)}
+        {
+            if (dim > 0)
+                j = index[0];
+            if (dim > 1)
+                k = index[1];
+        }
+
+        field_operator_base(std::size_t level, interval_t interval)
+            : level{level}, i{interval}, dx{1. / (1 << level)}
+        {}
+
+        field_operator_base(std::size_t level, interval_t interval,
+                            coord_index_t j_)
+            : level{level}, dx{1. / (1 << level)}, i{interval}, j{j_}
+        {}
+
+        field_operator_base(std::size_t level, interval_t interval,
+                            coord_index_t j_, coord_index_t k_)
+            : level{level}, dx{1. / (1 << level)}, i{interval}, j{j_}, k{k_}
+        {}
+    };
+
+#define INIT_OPERATOR(NAME)                                                    \
+    using interval_t = TInterval;                                              \
+    using coord_index_t = typename interval_t::coord_index_t;                  \
+                                                                               \
+    using base = field_operator_base<interval_t>;                              \
+    using base::i;                                                             \
+    using base::j;                                                             \
+    using base::k;                                                             \
+    using base::level;                                                         \
+    using base::dx;                                                            \
+                                                                               \
+    template<std::size_t dim>                                                  \
+    NAME(std::size_t level, interval_t interval,                               \
+         xt::xtensor_fixed<coord_index_t, xt::xshape<dim>> index)              \
+        : base(level, interval, index)                                         \
+    {}                                                                         \
+    template<class... index_t>                                                 \
+    NAME(std::size_t level, interval_t interval, index_t... index)             \
+        : base(level, interval, index...)                                      \
+    {}
+}
