543cddd7609eb5b1e6dc07532da0ead8659eb568
gouarin <loic.gouarin@gmail.com>
2020-02-12 15:14:33 +0100

    update bench 2D

--- demos/bench_laplacian_2d.cpp
+++ demos/bench_laplacian_2d.cpp
@@ -9,9 +9,7 @@
 #include <xtensor/xview.hpp>
 #include "xtensor/xnoalias.hpp"
 
-#include <mure/box.hpp>
-#include <mure/level_cell_array.hpp>
-#include <mure/mr_config.hpp>
+#include <mure/mure.hpp>
 
 /// Timer used in tic & toc
 auto tic_timer = std::chrono::high_resolution_clock::now();
@@ -45,41 +43,44 @@ int main(int argc, char* argv[])
     if (argc > 2)
         nrun = std::stoull(argv[2]);
 
-    const size_t end = std::pow(2, level);
+    const size_t end = std::pow(2, level) +2;
     using Config = mure::MRConfig<dim>;
-    mure::Box<int, dim> box({0, 0}, {(int) end, (int) end});
+    mure::Box<double, dim> box({0, 0}, {1, 1});
+    mure::Mesh<Config> mesh{box, 0, level};
 
-
-    mure::LevelCellArray<Config> lca = {box};
-
-    auto array_1 = xt::xtensor<double, 1>::from_shape({lca.nb_cells()});
-    array_1.fill(1.);
-    auto array_2 = xt::xtensor<double, 1>::from_shape({lca.nb_cells()});
+    mure::Field<Config> array_1{"array_1", mesh};
+    array_1.array().fill(1.);
+    mure::Field<Config> array_2{"array_2", mesh};
+    array_2.array().fill(0.);
 
     std::cout << "Mure:\n";
     for(size_t n=0; n<nrun; ++n)
     {
         tic();
-        lca.for_each_block([&](auto & load_input, auto & load_output){
-            auto view = load_input(array_1);
+        auto subset = mure::intersection(mesh[mure::MeshType::cells][level],
+                                         mesh[mure::MeshType::cells][level]);
 
-            xt::noalias(load_output(array_2)) =
-                2*xt::view(view, xt::range(1, view.shape()[0]-1), xt::range(1, view.shape()[1]-1))
-                -   xt::view(view, xt::range(2, view.shape()[0]), xt::range(1, view.shape()[1]-1))
-                -   xt::view(view, xt::range(0, view.shape()[0]-2), xt::range(1, view.shape()[1]-1))
-                -   xt::view(view, xt::range(1, view.shape()[0]-1), xt::range(2, view.shape()[1]))
-                -   xt::view(view, xt::range(1, view.shape()[0]-1), xt::range(0, view.shape()[1]-2));
-
+        subset([&](auto &index, auto &interval, auto)
+        {
+            auto i = interval[0];
+            auto j = index[0];
+            xt::noalias(array_2(level, i, j)) = 2.*array_1(level, i , j)
+                                              - array_1(level, i - 1 , j)
+                                              - array_1(level, i + 1 , j)                                 
+                                              - array_1(level, i     , j - 1)
+                                              - array_1(level, i     , j + 1);                                 
         });
         auto duration = toc();
 
-        std::cout << "\tRun #" << n << " in " << duration << "s (" << xt::sum(array_2) << ")\n";
+        std::cout << "\tRun #" << n << " in " << duration << "s (" << xt::sum(array_2.array()) << ")\n";
     }
 
 
-    auto view = xt::reshape_view(array_1, {end, end});
-    auto array_3 = xt::xtensor<double, 1>::from_shape({lca.nb_cells()});
+    auto array_3 = xt::xtensor<double, 1>::from_shape({end*end});
     auto array_3_ = xt::reshape_view(array_3, {end, end});
+    auto array_4 = xt::xtensor<double, 1>::from_shape({end*end});
+    auto array_4_ = xt::reshape_view(array_4, {end, end});
+    array_4_.fill(1.);
 
     std::cout << "xtensor:\n";
     for(size_t n=0; n<nrun; ++n)
@@ -87,18 +88,18 @@ int main(int argc, char* argv[])
         tic();
 
         xt::noalias(xt::view(array_3_, xt::range(1, array_3_.shape()[0]-1), xt::range(1, array_3_.shape()[1]-1))) =
-                2*xt::view(view, xt::range(1, view.shape()[0]-1), xt::range(1, view.shape()[1]-1))
-            -   xt::view(view, xt::range(2, view.shape()[0]), xt::range(1, view.shape()[1]-1))
-            -   xt::view(view, xt::range(0, view.shape()[0]-2), xt::range(1, view.shape()[1]-1))
-            -   xt::view(view, xt::range(1, view.shape()[0]-1), xt::range(2, view.shape()[1]))
-            -   xt::view(view, xt::range(1, view.shape()[0]-1), xt::range(0, view.shape()[1]-2));
+                2*xt::view(array_4_, xt::range(1, array_4_.shape()[0]-1), xt::range(1, array_4_.shape()[1]-1))
+            -   xt::view(array_4_, xt::range(2, array_4_.shape()[0]), xt::range(1, array_4_.shape()[1]-1))
+            -   xt::view(array_4_, xt::range(0, array_4_.shape()[0]-2), xt::range(1, array_4_.shape()[1]-1))
+            -   xt::view(array_4_, xt::range(1, array_4_.shape()[0]-1), xt::range(2, array_4_.shape()[1]))
+            -   xt::view(array_4_, xt::range(1, array_4_.shape()[0]-1), xt::range(0, array_4_.shape()[1]-2));
 
         auto duration = toc();
         std::cout << "\tRun #" << n << " in " << duration << "s (" << xt::sum(array_3) << ")\n";
     }
 
-    std::vector<double> vector_1(lca.nb_cells(), 1.);
-    std::vector<double> vector_2(lca.nb_cells(), 0  );
+    std::vector<double> vector_1(end*end, 1.);
+    std::vector<double> vector_2(end*end, 0  );
 
     std::cout << "std::vector:\n";
     for(size_t n=0; n<nrun; ++n)
