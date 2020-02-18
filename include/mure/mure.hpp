350d1ce110a92b5c3fe67af19e19307e05b13986
gouarin <loic.gouarin@gmail.com>
2020-02-18 10:08:38 +0100

    add mure.hpp

---
+++ include/mure/mure.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "box.hpp"
+#include "field.hpp"
+#include "hdf5.hpp"
+#include "mr/adapt.hpp"
+#include "mr/mesh.hpp"
+#include "mr/mr_config.hpp"
+#include "mr/pred_and_proj.hpp"
