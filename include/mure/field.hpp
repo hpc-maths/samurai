b317967a415925884f8ebbbf2dd8f8183f9e41f0
gouarin <loic.gouarin@gmail.com>
2020-03-27 10:59:19 +0100

    fix add_field_on_level

--- include/mure/field.hpp
+++ include/mure/field.hpp
@@ -246,7 +246,7 @@ namespace mure
             m_mesh->for_each_cell(level,
                                   [&](auto cell)
                                   {
-                                      output[index++] = xt::view(m_data, cell.index);
+                                      xt::view(output, index++) = xt::view(m_data, cell.index);
                                   },
                                   mesh_type);
             return output;
