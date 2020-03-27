ac6d82219c96be5bb00d569a33684911786445e7
Thomas Bellotti <thomas.bellotti@polytechnique.edu>
2020-03-27 09:02:26 +0100

    adapted _add_on_level in order to deal with multiple fields

--- include/mure/hdf5.hpp
+++ include/mure/hdf5.hpp
@@ -240,6 +240,7 @@ namespace mure
             }
         }
 
+
         template<class MRConfig, class Field>
         inline void _add_on_level(const Mesh<MRConfig> &mesh,
                                   const Field &field,
@@ -326,28 +327,154 @@ namespace mure
                     geom_data.append_attribute("Format") = "HDF";
                     geom_data.text() = (filename + ".h5:" + ss2.str()).c_str();
 
-                    std::stringstream ss;
-                    ss << "level/" << level << "/" << mesh_type << "/fields/"
-                       << field.name();
-                    xt::dump(h5_file, ss.str().data(),
-                             field.data_on_level(
-                                 level, static_cast<MeshType>(mesh_type)));
+                    // Modified to handle multiple components of the field
+                    auto data_tmp = field.data(static_cast<MeshType>(mesh_type));
 
-                    auto attribute = grid.append_child("Attribute");
-                    attribute.append_attribute("Name") = field.name().c_str();
-                    attribute.append_attribute("Center") = "Cell";
-                    auto dataitem = attribute.append_child("DataItem");
-                    dataitem.append_attribute("Dimensions") =
-                        field.nb_cells(level, static_cast<MeshType>(mesh_type));
-                    dataitem.append_attribute("Format") = "HDF";
-                    dataitem.text() =
-                        (filename + ".h5:/level/" + std::to_string(level) +
-                         "/" + std::to_string(mesh_type) + "/fields/" +
-                         field.name())
-                            .c_str();
+                    for (std::size_t h = 0; h < data_tmp.shape()[1]; ++h)   {
+
+                        std::stringstream new_field_name;
+                        new_field_name << field.name()<< "_" << h;
+
+                        std::stringstream ss;
+                        ss << "level/" << level << "/" << mesh_type << "/fields/"
+                           << new_field_name.str();
+
+                        auto all_fields = field.data_on_level(
+                                 level, static_cast<MeshType>(mesh_type));
+                        
+                        xt::dump(h5_file, ss.str().data(), xt::eval(xt::view(all_fields, xt::all(), h)));
+
+                        auto attribute = grid.append_child("Attribute");
+                        attribute.append_attribute("Name") = new_field_name.str().c_str();//field.name().c_str();
+                        attribute.append_attribute("Center") = "Cell";
+                        auto dataitem = attribute.append_child("DataItem");
+                        dataitem.append_attribute("Dimensions") =
+                            field.nb_cells(level, static_cast<MeshType>(mesh_type));
+                        dataitem.append_attribute("Format") = "HDF";
+                        dataitem.text() =
+                            ((filename + ".h5:/level/" + std::to_string(level) +
+                             "/" + std::to_string(mesh_type) + "/fields/" +
+                             new_field_name.str()).c_str());
+                        
+                        //dataitem.text() =
+                        //(filename + ".h5:/level/" + std::to_string(level) +
+                        // "/" + std::to_string(mesh_type) + "/fields/" +
+                        // field.name())
+                        //    .c_str();
+                    }
+
                 }
             }
         }
+
+        // template<class MRConfig, class Field>
+        // inline void _add_on_level(const Mesh<MRConfig> &mesh,
+        //                           const Field &field,
+        //                           std::size_t level)
+        // {
+        //     std::size_t nb_points = std::pow(2, Mesh<MRConfig>::dim);
+        //     constexpr std::size_t dim = Mesh<MRConfig>::dim;
+
+        //     std::array<std::string, 5> mesh_name{"cells", "cells_and_ghosts",
+        //                                          "proj", "all", "union"};
+
+        //     auto range = xt::arange(nb_points);
+
+        //     auto grid_parent = domain.append_child("Grid");
+        //     grid_parent.append_attribute("Name") =
+        //         ("level " + std::to_string(level)).c_str();
+        //     grid_parent.append_attribute("GridType") = "Collection";
+
+        //     for (std::size_t mesh_type = 0; mesh_type < 5; ++mesh_type)
+        //     {
+        //         if (mesh.nb_cells(level, static_cast<MeshType>(mesh_type)) != 0)
+        //         {
+        //             xt::xtensor<std::size_t, 2> connectivity;
+        //             connectivity.resize(
+        //                 {mesh.nb_cells(level, static_cast<MeshType>(mesh_type)),
+        //                  nb_points});
+
+        //             xt::xtensor<double, 2> coords;
+        //             coords.resize(
+        //                 {nb_points * mesh.nb_cells(level, static_cast<MeshType>(
+        //                                                       mesh_type)),
+        //                  3});
+        //             coords.fill(0);
+
+        //             auto element =
+        //                 get_element(std::integral_constant<std::size_t, dim>{});
+
+        //             std::size_t index = 0;
+        //             mesh.for_each_cell(
+        //                 level,
+        //                 [&](auto cell) {
+        //                     auto coords_view =
+        //                         xt::view(coords,
+        //                                  xt::range(nb_points * index,
+        //                                            nb_points * (index + 1)),
+        //                                  xt::range(0, dim));
+        //                     auto connectivity_view =
+        //                         xt::view(connectivity, index, xt::all());
+
+        //                     coords_view = xt::eval(cell.first_corner() +
+        //                                            cell.length() * element);
+        //                     connectivity_view =
+        //                         xt::eval(nb_points * index + range);
+        //                     index++;
+        //                 },
+        //                 static_cast<MeshType>(mesh_type));
+        //             std::stringstream ss1;
+        //             ss1 << "level/" << level << "/mesh/" << mesh_type
+        //                 << "/connectivity";
+        //             xt::dump(h5_file, ss1.str().data(), connectivity);
+        //             std::stringstream ss2;
+        //             ss2 << "level/" << level << "/mesh/" << mesh_type
+        //                 << "/points";
+        //             xt::dump(h5_file, ss2.str().data(), coords);
+
+        //             auto grid = grid_parent.append_child("Grid");
+        //             grid.append_attribute("Name") =
+        //                 mesh_name[mesh_type].c_str();
+        //             auto topo = grid.append_child("Topology");
+        //             topo.append_attribute("TopologyType") =
+        //                 element_type(dim).c_str();
+        //             topo.append_attribute("NumberOfElements") =
+        //                 connectivity.shape()[0];
+        //             auto topo_data = topo.append_child("DataItem");
+        //             topo_data.append_attribute("Dimensions") =
+        //                 connectivity.size();
+        //             topo_data.append_attribute("Format") = "HDF";
+        //             topo_data.text() = (filename + ".h5:" + ss1.str()).c_str();
+
+        //             auto geom = grid.append_child("Geometry");
+        //             geom.append_attribute("GeometryType") = "XYZ";
+        //             auto geom_data = geom.append_child("DataItem");
+        //             geom_data.append_attribute("Dimensions") = coords.size();
+        //             geom_data.append_attribute("Format") = "HDF";
+        //             geom_data.text() = (filename + ".h5:" + ss2.str()).c_str();
+
+        //             std::stringstream ss;
+        //             ss << "level/" << level << "/" << mesh_type << "/fields/"
+        //                << field.name();
+        //             xt::dump(h5_file, ss.str().data(),
+        //                      field.data_on_level(
+        //                          level, static_cast<MeshType>(mesh_type)));
+
+        //             auto attribute = grid.append_child("Attribute");
+        //             attribute.append_attribute("Name") = field.name().c_str();
+        //             attribute.append_attribute("Center") = "Cell";
+        //             auto dataitem = attribute.append_child("DataItem");
+        //             dataitem.append_attribute("Dimensions") =
+        //                 field.nb_cells(level, static_cast<MeshType>(mesh_type));
+        //             dataitem.append_attribute("Format") = "HDF";
+        //             dataitem.text() =
+        //                 (filename + ".h5:/level/" + std::to_string(level) +
+        //                  "/" + std::to_string(mesh_type) + "/fields/" +
+        //                  field.name())
+        //                     .c_str();
+        //         }
+        //     }
+        // }
 
       private:
         HighFive::File h5_file;
