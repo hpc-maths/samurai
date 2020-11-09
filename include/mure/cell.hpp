0b825956ddaef9a750b941416dc5acca32c849af
gouarin <loic.gouarin@gmail.com>
2020-11-09 10:57:41 +0100

    remove center computation during cell constructor for better performance

--- include/mure/cell.hpp
+++ include/mure/cell.hpp
@@ -16,7 +16,7 @@ namespace mure
     template<class TCoord_index, std::size_t dim_>
     struct Cell
     {
-        static constexpr auto dim = dim_;
+        static constexpr std::size_t dim = dim_;
         using coord_index_t = TCoord_index;
 
         Cell() = default;
@@ -29,13 +29,15 @@ namespace mure
         template <class T>
         Cell(std::size_t level, const T& indices, std::size_t index);
 
-        xt::xtensor_fixed<double, xt::xshape<dim>> first_corner() const;
+        xt::xtensor_fixed<double, xt::xshape<dim>> corner() const;
+        double corner(std::size_t i) const;
 
         /// The integer coordinates of the cell.
         xt::xtensor_fixed<coord_index_t, xt::xshape<dim>> indices;
 
         /// The center of the cell.
-        xt::xtensor_fixed<double, xt::xshape<dim>> center;
+        xt::xtensor_fixed<double, xt::xshape<dim>> center() const;
+        double center(std::size_t i) const;
 
         /// The level of the cell.
         std::size_t level;
@@ -55,16 +57,37 @@ namespace mure
     : level(level), indices(indices), index(index)
     {
         length = 1./(1 << level);
-        center = length*(indices + 0.5);
+        // center = length*(indices + 0.5);
     }
 
     /**
      * The minimum corner of the cell.
      */
     template<class TCoord_index, std::size_t dim_>
-    inline auto Cell<TCoord_index, dim_>::first_corner() const -> xt::xtensor_fixed<double, xt::xshape<dim>>
+    inline auto Cell<TCoord_index, dim_>::corner() const -> xt::xtensor_fixed<double, xt::xshape<dim>>
     {
-        return xt::eval(length*indices);
+        return length*indices;
+    }
+
+    template<class TCoord_index, std::size_t dim_>
+    inline double Cell<TCoord_index, dim_>::corner(std::size_t i) const
+    {
+        return length*indices[i];
+    }
+
+    /**
+     * The minimum corner of the cell.
+     */
+    template<class TCoord_index, std::size_t dim_>
+    inline auto Cell<TCoord_index, dim_>::center() const -> xt::xtensor_fixed<double, xt::xshape<dim>>
+    {
+        return length*(indices + 0.5);
+    }
+
+    template<class TCoord_index, std::size_t dim_>
+    inline double Cell<TCoord_index, dim_>::center(std::size_t i) const
+    {
+        return length*(indices[i] + 0.5);
     }
 
     template<class TCoord_index, std::size_t dim_>
