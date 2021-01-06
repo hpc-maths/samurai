5249db263dd586f22107e345a90adfcaa40ddd2c
gouarin <loic.gouarin@gmail.com>
2021-01-06 15:13:23 +0100

    add xml generation into readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -209,4 +209,4 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('cd ..; doxygen', shell=True)
