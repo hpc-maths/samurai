88adc793fd998854b758b4fd3bca0926404355d7
gouarin <loic.gouarin@gmail.com>
2021-01-06 15:06:53 +0100

    add xml generation into readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -200,3 +200,13 @@ epub_exclude_files = ['search.html']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+# -- Breathe configuration for readthedocs -----------------------------------
+
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+    subprocess.call('cd ../doxygen; doxygen', shell=True)
