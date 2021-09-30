9d1f5413ded528bade8f3860fc7ee63d5b236744
gouarin <loic.gouarin@gmail.com>
2021-09-30 14:19:32 +0200

    fix include

--- demos/tutorial/graduation_case_1.cpp
+++ demos/tutorial/graduation_case_1.cpp
@@ -11,6 +11,7 @@
 #include <samurai/cell_array.hpp>
 #include <samurai/field.hpp>
 #include <samurai/hdf5.hpp>
+#include <samurai/subset/subset_op.hpp>
 
 auto generate_mesh(std::size_t start_level, std::size_t max_level)
 {
