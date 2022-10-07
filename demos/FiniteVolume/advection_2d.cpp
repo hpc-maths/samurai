cdd13533c8ef1926901c3506ababc5129bdd19a6
Loic Gouarin <loic.gouarin@gmail.com>
2022-10-07 14:45:34 +0200

    add pytest installation

--- demos/FiniteVolume/advection_2d.cpp
+++ demos/FiniteVolume/advection_2d.cpp
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 #include <array>
+#include "CLI/CLI.hpp"
+
+#include <xtensor/xfixed.hpp>
 
 #include <samurai/field.hpp>
 #include <samurai/mr/mesh.hpp>
@@ -11,7 +14,8 @@
 #include <samurai/hdf5.hpp>
 #include <samurai/subset/subset_op.hpp>
 
-#include <xtensor/xfixed.hpp>
+#include <filesystem>
+namespace fs = std::filesystem;
 
 template <class Mesh>
 auto init(Mesh& mesh)
@@ -137,17 +141,22 @@ void dirichlet(std::size_t level, Field& u)
 }
 
 template <class Field>
-void save(std::size_t nt, const Field& u)
+void save(const fs::path& path, const std::string& filename, const Field& u, const std::string& suffix="")
 {
     auto mesh = u.mesh();
     auto level_ = samurai::make_field<std::size_t, 1>("level", mesh);
+
+    if (!fs::exists(path))
+    {
+        fs::create_directory(path);
+    }
 
     samurai::for_each_cell(mesh, [&](auto &cell)
     {
         level_[cell] = cell.level;
     });
 
-    samurai::save(fmt::format("FV_advection_2d_ite_{}", nt), mesh, u, level_);
+    samurai::save(fmt::format("{}/{}{}", path.string(), filename, suffix), mesh, u, level_);
 }
 
 int main(int argc, char *argv[])
@@ -156,15 +165,44 @@ int main(int argc, char *argv[])
     using Config = samurai::MRConfig<dim>;
     using interval_t = typename Config::interval_t;
 
-    const double regularity = 1.;    // Regularity guess for multiresolution
-    const double epsilon_MR = 2.e-4; // Threshold used by multiresolution
+    // Simulation parameters
+    double left_box = {0., 0.}, right_box = {1., 1.};
+    std::array<double, 2> a{{1, 1}};
+    double Tf = 1.;
+    double cfl = 0.5;
 
+    // Multiresolution parameters
     std::size_t min_level = 4, max_level = 10;
+    double mr_epsilon = 2.e-4; // Threshold used by multiresolution
+    double mr_regularity = 1.; // Regularity guess for multiresolution
+    bool correction = false;
+
+    // Output parameters
+    fs::path path = fs::current_path();
+    std::string filename = "FV_advection_2d";
+    std::size_t nfiles = 1;
+
+    CLI::App app{"Finite volume 1d example"};
+    app.add_option("--left", left_box, "The left border of the box")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--right", right_box, "The right border of the box")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--velocity", a, "The velocity of the advection equation")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--cfl", cfl, "The CFL")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--Tf", Tf, "Final time")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--min-level", min_level, "Minimum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--max-level", max_level, "Maximum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--mr-eps", mr_epsilon, "The epsilon used by the multiresolution to adapt the mesh")->capture_default_str()->group("Multiresolution");
+    app.add_option("--mr-reg", mr_regularity, "The regularity criteria used by the multiresolution to adapt the mesh")->capture_default_str()->group("Multiresolution");
+    app.add_option("--with-correction", correction, "Apply flux correction at the interface of two refinement levels")->capture_default_str()->group("Multiresolution");
+    app.add_option("--path", path, "Output path")->capture_default_str()->group("Ouput");
+    app.add_option("--filename", filename, "File name prefix")->capture_default_str()->group("Ouput");
+    app.add_option("--nfiles", nfiles,  "Number of output files")->capture_default_str()->group("Ouput");
+    CLI11_PARSE(app, argc, argv);
+
+
     samurai::Box<double, dim> box({0, 0}, {1, 1});
     samurai::MRMesh<Config> mesh{box, min_level, max_level};
 
-    std::array<double, 2> a{{1, 1}};
-    double dt = .5/(1<<max_level);
+    double dt = cfl/(1<<max_level);
 
     auto u = init(mesh);
     auto unp1 = samurai::make_field<double, 1>("unp1", mesh);
@@ -175,24 +213,37 @@ int main(int argc, char *argv[])
     };
 
     auto MRadaptation = samurai::make_MRAdapt(u, update_bc);
+    MRadaptation(mr_epsilon, mr_regularity);
+    save(path, filename, u, "_init");
 
-    for (std::size_t nt=0; nt<500; ++nt)
+    while (t != Tf)
     {
-        std::cout << "iteration " << nt << "\n";
+        MRadaptation(mr_epsilon, mr_regularity);
 
-        if (max_level > min_level)
+        t += dt;
+        if (t > Tf)
         {
-            MRadaptation(epsilon_MR, regularity);
+            dt += Tf - t;
+            t = Tf;
         }
+
+        std::cout << fmt::format("iteration {}: t = {}, dt = {}", nt++, t, dt) << std::endl;
 
         samurai::update_ghost_mr(u, update_bc);
         unp1.resize();
         unp1 = u - dt * samurai::upwind(a, u);
-        // flux_correction(dt, a, u, unp1);
+        if (correction)
+        {
+            flux_correction(dt, a, u, unp1);
+        }
 
         std::swap(u.array(), unp1.array());
 
-        save(nt, u);
+        if ( t >= (nsave+1)*dt_save || t == Tf)
+        {
+            std::string suffix = (nfiles!=1)? fmt::format("_ite_{}", nsave++): "";
+            save(path, filename, u, suffix);
+        }
     }
     return 0;
 }
