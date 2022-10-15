6495baf20653aa92a435fa2780703aa54fa6f2c1
Loic Gouarin <loic.gouarin@gmail.com>
2022-10-15 14:17:15 +0200

    update step 3

--- demos/tutorial/AMR_1D_Burgers/step_3/update_sol.hpp
+++ demos/tutorial/AMR_1D_Burgers/step_3/update_sol.hpp
@@ -10,13 +10,13 @@ template<class Field>
 void update_sol(double dt, Field& phi, Field& phi_np1)
 {
     auto mesh = phi.mesh();
-    using mesh_id_t = typename decltype(mesh)::mesh_id_t;                                                           // <-----------------
+    using mesh_id_t = typename decltype(mesh)::mesh_id_t;                                                                 // <-----------------
 
-    samurai::for_each_interval(mesh[mesh_id_t::cells], [&](std::size_t level, const auto& i, auto)                  // <-----------------
+    samurai::for_each_interval(mesh[mesh_id_t::cells], [&](std::size_t level, const auto& i, auto)                        // <-----------------
     {
         double dx = 1./(1<<level);
 
-        phi_np1(level, i) = phi(level, i) - .5*dt/dx*(xt::pow(phi(level, i), 2.) - xt::pow(phi(level, i - 1), 2.)); // <-----------------
+        phi_np1(level, i) = phi(level, i) - .5*dt/dx*(phi(level, i)*phi(level, i) - phi(level, i - 1)*phi(level, i - 1)); // <-----------------
     });
 
     std::swap(phi.array(), phi_np1.array());
