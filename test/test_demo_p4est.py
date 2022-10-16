4a09ded4a521d91b05a11863f543c96707f74d5b
Loic Gouarin <loic.gouarin@gmail.com>
2022-10-16 13:12:19 +0200

    add pytest for p4est

---
+++ test/test_demo_p4est.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+import subprocess
+
+path = 'p4est'
+
+@pytest.fixture
+def config():
+    return {'path': path}
+
+def get_executable(path, filename):
+    if os.path.exists(os.path.join(path, filename)):
+        return os.path.join(path, filename)
+    return os.path.join(path, 'Release', filename)
+
+@pytest.mark.h5diff()
+def test_simple_2d(config):
+    cmd = [get_executable("../build/demos/p4est/", "p4est-simple-2d"),
+           "--path", config['path'],
+           '--filename', config['filename']]
+    output = subprocess.run(cmd, check=True, capture_output=True)
