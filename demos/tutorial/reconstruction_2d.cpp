ba04350af57a82630889f6865f78ff36f88dd6ef
Loic Gouarin <loic.gouarin@gmail.com>
2022-10-16 15:41:08 +0200

    add reconstruction ref

--- demos/tutorial/reconstruction_2d.cpp
+++ demos/tutorial/reconstruction_2d.cpp
@@ -1,3 +1,11 @@
+// Copyright 2021 SAMURAI TEAM. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+#include "CLI/CLI.hpp"
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
 #include <chrono>
 
 #include <samurai/mr/mesh.hpp>
@@ -8,13 +16,14 @@
 #include <samurai/reconstruction.hpp>
 #include <samurai/uniform_mesh.hpp>
 
+enum class Case : int { abs, exp, tanh };
+
 template <class Mesh>
-auto init(Mesh& mesh)
+auto init(Mesh& mesh, Case& c)
 {
     using mesh_id_t = typename Mesh::mesh_id_t;
 
     auto u = samurai::make_field<double, 1>("u", mesh);
-
 
     samurai::for_each_interval(mesh[mesh_id_t::cells], [&](std::size_t level, auto& i, auto& index)
     {
@@ -22,18 +31,27 @@ auto init(Mesh& mesh)
         double dx = 1./(1<<level);
         auto x = dx*xt::arange(i.start, i.end) + 0.5*dx;
         auto y = j*dx + 0.5*dx;
-        // u(level, i) = xt::exp(-100*(x*x + y*y));
 
-        u(level, i, j) = xt::abs(x) + std::abs(y);
+        switch(c)
+        {
+            case Case::abs:
+                u(level, i, j) = xt::abs(x) + std::abs(y);
+                break;
+            case Case::exp:
+                u(level, i, j) = xt::exp(-100*(x*x + y*y));
+                break;
+            case Case::tanh:
+                u(level, i, j) = xt::tanh(50*(xt::abs(x) + std::abs(y))) - 1;
+                break;
+        }
     });
     return u;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     constexpr size_t dim = 2;
     constexpr std::size_t max_stencil_width_ = 1;
-    // constexpr std::size_t graduation_width_ = samurai::default_config::graduation_width;
     constexpr std::size_t graduation_width_ = 2;
     constexpr std::size_t max_refinement_level_ = samurai::default_config::max_level;
     constexpr std::size_t prediction_order_ = 1;
@@ -42,36 +60,58 @@ int main()
         max_refinement_level_,
         prediction_order_
     >;
+
+    Case test_case{Case::abs};
+    std::map<std::string, Case> map{{"abs", Case::abs}, {"exp", Case::exp}, {"tanh", Case::tanh}};
+
+    // Adaptation parameters
+    std::size_t min_level = 3, max_level = 8;
+    double mr_epsilon = 1.e-4; // Threshold used by multiresolution
+    double mr_regularity = 2.; // Regularity guess for multiresolution
+
+    // Output parameters
+    fs::path path = fs::current_path();
+    std::string filename = "reconstruction_2d";
+
+    CLI::App app{"1d reconstruction of an adapted solution using multiresolution"};
+    app.add_option("--case", test_case, "Test case")->capture_default_str()->transform(CLI::CheckedTransformer(map, CLI::ignore_case));
+    app.add_option("--min-level", min_level, "Minimum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--max-level", max_level, "Maximum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--mr-eps", mr_epsilon, "The epsilon used by the multiresolution to adapt the mesh")->capture_default_str()->group("Multiresolution");
+    app.add_option("--mr-reg", mr_regularity, "The regularity criteria used by the multiresolution to adapt the mesh")->capture_default_str()->group("Multiresolution");
+    app.add_option("--path", path, "Output path")->capture_default_str()->group("Ouput");
+    app.add_option("--filename", filename, "File name prefix")->capture_default_str()->group("Ouput");
+    CLI11_PARSE(app, argc, argv);
+
+    if (!fs::exists(path))
+    {
+        fs::create_directory(path);
+    }
+
     using MRMesh = samurai::MRMesh<MRConfig>;
     using mrmesh_id_t = typename MRMesh::mesh_id_t;
 
     using UConfig = samurai::UniformConfig<dim>;
     using UMesh = samurai::UniformMesh<UConfig>;
 
-    std::size_t min_level = 2, max_level = 10;
     samurai::Box<double, dim> box({-1, -1}, {1, 1});
     MRMesh mrmesh {box, min_level, max_level};
     UMesh umesh {box, max_level};
-    auto u = init(mrmesh);
-    auto u_exact = init(umesh);
+    auto u = init(mrmesh, test_case);
+    auto u_exact = init(umesh, test_case);
 
     auto update_bc_for_level = [](auto&, std::size_t)
     {
     };
 
     auto MRadaptation = samurai::make_MRAdapt(u, update_bc_for_level);
-
-    MRadaptation(1e-4, 2);
+    MRadaptation(mr_epsilon, mr_regularity);
 
     auto level_ = samurai::make_field<std::size_t, 1>("level", mrmesh);
-
     samurai::for_each_cell(mrmesh[mrmesh_id_t::cells], [&](auto &cell) {
         level_[cell] = cell.level;
     });
-
-    samurai::update_ghost_mr(u, update_bc_for_level);
-
-    samurai::save("solution", mrmesh, u, level_);
+    samurai::save(path, filename, mrmesh, u, level_);
 
     auto t1 = std::chrono::high_resolution_clock::now();
     auto u_reconstruct = reconstruction(u);
@@ -85,8 +125,7 @@ int main()
         error(level, i, j) = xt::abs(u_reconstruct(level, i, j) - u_exact(level, i, j));
 
     });
-
-    samurai::save("solution_uniform", u_reconstruct.mesh(), u_reconstruct, error);
+    samurai::save(path, fmt::format("uniform_{}", filename), u_reconstruct.mesh(), u_reconstruct, error);
 
     return 0;
 }
--- demos/tutorial/reconstruction_3d.cpp
+++ demos/tutorial/reconstruction_3d.cpp
@@ -1,3 +1,11 @@
+// Copyright 2021 SAMURAI TEAM. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+#include "CLI/CLI.hpp"
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
 #include <chrono>
 
 #include <samurai/mr/mesh.hpp>
@@ -8,8 +16,10 @@
 #include <samurai/reconstruction.hpp>
 #include <samurai/uniform_mesh.hpp>
 
+enum class Case : int { abs, exp, tanh };
+
 template <class Mesh>
-auto init(Mesh& mesh)
+auto init(Mesh& mesh, Case& c)
 {
     using mesh_id_t = typename Mesh::mesh_id_t;
 
@@ -24,14 +34,24 @@ auto init(Mesh& mesh)
         auto x = dx*xt::arange(i.start, i.end) + 0.5*dx;
         auto y = j*dx + 0.5*dx;
         auto z = k*dx + 0.5*dx;
-        u(level, i, j, k) = xt::exp(-100*(x*x + y*y + z*z));
-        // xt::tanh(50*xt::abs(x)) - 1
-        // u(level, i, j, k) = xt::abs(x) + std::abs(y) + std::abs(z);
+
+        switch(c)
+        {
+            case Case::abs:
+                u(level, i, j, k) = xt::abs(x) + std::abs(y) + std::abs(z);
+                break;
+            case Case::exp:
+                u(level, i, j, k) = xt::exp(-100*(x*x + y*y + z*z));
+                break;
+            case Case::tanh:
+                u(level, i, j, k) = xt::tanh(50*(xt::abs(x) + std::abs(y) + std::abs(z))) - 1;
+                break;
+        }
     });
     return u;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     constexpr size_t dim = 3;
     constexpr std::size_t max_stencil_width_ = 1;
@@ -44,36 +64,58 @@ int main()
         max_refinement_level_,
         prediction_order_
     >;
+
+    Case test_case{Case::abs};
+    std::map<std::string, Case> map{{"abs", Case::abs}, {"exp", Case::exp}, {"tanh", Case::tanh}};
+
+    // Adaptation parameters
+    std::size_t min_level = 2, max_level = 5;
+    double mr_epsilon = 1.e-4; // Threshold used by multiresolution
+    double mr_regularity = 2.; // Regularity guess for multiresolution
+
+    // Output parameters
+    fs::path path = fs::current_path();
+    std::string filename = "reconstruction_3d";
+
+    CLI::App app{"1d reconstruction of an adapted solution using multiresolution"};
+    app.add_option("--case", test_case, "Test case")->capture_default_str()->transform(CLI::CheckedTransformer(map, CLI::ignore_case));
+    app.add_option("--min-level", min_level, "Minimum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--max-level", max_level, "Maximum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--mr-eps", mr_epsilon, "The epsilon used by the multiresolution to adapt the mesh")->capture_default_str()->group("Multiresolution");
+    app.add_option("--mr-reg", mr_regularity, "The regularity criteria used by the multiresolution to adapt the mesh")->capture_default_str()->group("Multiresolution");
+    app.add_option("--path", path, "Output path")->capture_default_str()->group("Ouput");
+    app.add_option("--filename", filename, "File name prefix")->capture_default_str()->group("Ouput");
+    CLI11_PARSE(app, argc, argv);
+
+    if (!fs::exists(path))
+    {
+        fs::create_directory(path);
+    }
+
     using MRMesh = samurai::MRMesh<MRConfig>;
     using mrmesh_id_t = typename MRMesh::mesh_id_t;
 
     using UConfig = samurai::UniformConfig<dim>;
     using UMesh = samurai::UniformMesh<UConfig>;
 
-    std::size_t min_level = 2, max_level = 6;
     samurai::Box<double, dim> box({-1, -1, -1}, {1, 1, 1});
     MRMesh mrmesh {box, min_level, max_level};
     UMesh umesh {box, max_level};
-    auto u = init(mrmesh);
-    auto u_exact = init(umesh);
+    auto u = init(mrmesh, test_case);
+    auto u_exact = init(umesh, test_case);
 
     auto update_bc_for_level = [](auto&, std::size_t)
     {
     };
 
     auto MRadaptation = samurai::make_MRAdapt(u, update_bc_for_level);
-
-    MRadaptation(1e-4, 2);
+    MRadaptation(mr_epsilon, mr_regularity);
 
     auto level_ = samurai::make_field<std::size_t, 1>("level", mrmesh);
-
     samurai::for_each_cell(mrmesh[mrmesh_id_t::cells], [&](auto &cell) {
         level_[cell] = cell.level;
     });
-
-    samurai::update_ghost_mr(u, update_bc_for_level);
-
-    samurai::save("solution", mrmesh, u, level_);
+    samurai::save(path, filename, mrmesh, u, level_);
 
     auto t1 = std::chrono::high_resolution_clock::now();
     auto u_reconstruct = reconstruction(u);
@@ -88,8 +130,7 @@ int main()
         error(level, i, j, k) = xt::abs(u_reconstruct(level, i, j, k) - u_exact(level, i, j, k));
 
     });
-
-    samurai::save("solution_uniform", u_reconstruct.mesh(), u_reconstruct, error);
+    samurai::save(path, fmt::format("uniform_{}", filename), u_reconstruct.mesh(), u_reconstruct, error);
 
     return 0;
 }
