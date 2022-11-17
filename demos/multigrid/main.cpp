b63f62966fd609974e5e19d77bb1505155247ed0
Pierre Matalon <pierre.matalon@gmail.com>
2022-11-17 13:36:30 +0100

    Invert dim and stencil size in the definition of StencilShape

--- demos/multigrid/main.cpp
+++ demos/multigrid/main.cpp
@@ -111,7 +111,7 @@ Mesh create_refined_mesh(std::size_t n)
 
 int main(int argc, char* argv[])
 {
-    constexpr std::size_t dim = 2;
+    constexpr std::size_t dim = 1;
     using Config = samurai::amr::Config<dim>;
     using Mesh = samurai::amr::Mesh<Config>;
     //using Config = samurai::MRConfig<dim>;
