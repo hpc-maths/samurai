4414cdf2366005d63d99037e02e5eae7614e928d
Pierre Matalon <pierre.matalon@gmail.com>
2022-12-02 18:28:52 +0100

    New type of Dirichlet enforcement (#37)

--- include/samurai/petsc/petsc_assembly.hpp
+++ include/samurai/petsc/petsc_assembly.hpp
@@ -27,6 +27,7 @@ namespace samurai { namespace petsc
         void assemble_matrix(Mat& A)
         {
             assemble_scheme_on_uniform_grid(A);
+            assemble_boundary_conditions(A);
             assemble_projection(A);
             assemble_prediction(A);
 
@@ -63,6 +64,11 @@ namespace samurai { namespace petsc
         virtual void assemble_scheme_on_uniform_grid(Mat& A) = 0;
 
         /**
+         * @brief Inserts the coefficients into the matrix in order to enforce the boundary conditions.
+        */
+        virtual void assemble_boundary_conditions(Mat& A) = 0;
+
+        /**
          * @brief Inserts the coefficients corresponding to the projection operator into the matrix.
         */
         virtual void assemble_projection(Mat& A) = 0;
@@ -74,12 +80,22 @@ namespace samurai { namespace petsc
     };
 
 
+    enum DirichletEnforcement : int
+    {
+        Equation,
+        Elimination
+    };
+
+    /**
+     * Useful sizes to define the sparsity pattern of the matrix and perform the preallocation.
+    */
     template <PetscInt scheme_stencil_size_,
               PetscInt proj_stencil_size_,
               PetscInt pred_stencil_size_,
               PetscInt center_index_,
               PetscInt contiguous_indices_start_,
-              PetscInt contiguous_indices_size_>
+              PetscInt contiguous_indices_size_,
+              DirichletEnforcement dirichlet_enfcmt_ = Equation>
     struct PetscAssemblyConfig
     {
         static constexpr PetscInt scheme_stencil_size = scheme_stencil_size_;
@@ -88,5 +104,43 @@ namespace samurai { namespace petsc
         static constexpr PetscInt center_index = center_index_;
         static constexpr PetscInt contiguous_indices_start = contiguous_indices_start_;
         static constexpr PetscInt contiguous_indices_size = contiguous_indices_size_;
+        static constexpr DirichletEnforcement dirichlet_enfcmt = dirichlet_enfcmt_;
     };
+
+
+
+    
+    template<std::size_t dim, DirichletEnforcement dirichlet_enfcmt = Equation>
+    using starStencilFV = PetscAssemblyConfig
+    <
+        // ----  Stencil size 
+        // Cell-centered Finite Volume scheme:
+        // center + 1 neighbour in each Cartesian direction (2*dim directions) --> 1+2=3 in 1D
+        //                                                                         1+4=5 in 2D
+        1 + 2*dim,
+
+        // ----  Projection stencil size
+        // cell + 2^dim children --> 1+2=3 in 1D 
+        //                           1+4=5 in 2D
+        1 + (1 << dim), 
+
+        // ----  Prediction stencil size
+        // Here, order 1:
+        // cell + hypercube of 3 cells --> 1+3= 4 in 1D
+        //                                 1+9=10 in 2D
+        1 + ce_pow(3, dim), 
+
+        // ---- Index of the stencil center
+        // (as defined in star_stencil())
+        1, 
+
+        // ---- Start index and size of contiguous cell indices
+        // (as defined in star_stencil())
+        // Here, [left, center, right].
+        0, 3,
+
+        // ---- Method of Dirichlet condition enforcement
+        dirichlet_enfcmt
+    >;
+
 }} // end namespace
