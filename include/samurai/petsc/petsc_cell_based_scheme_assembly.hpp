a258d14d1d76643d0964e5e7fbd7885198eb440c
Pierre Matalon <pierre.matalon@gmail.com>
2023-01-12 07:23:38 +0100

    Vector field with adapted mesh

--- include/samurai/petsc/petsc_cell_based_scheme_assembly.hpp
+++ include/samurai/petsc/petsc_cell_based_scheme_assembly.hpp
@@ -506,7 +506,175 @@ namespace samurai { namespace petsc
 
         void assemble_prediction(Mat& A) const override
         {
-            assemble_prediction_impl(std::integral_constant<std::size_t, dim>{}, A, _mesh);
+            static_assert(dim >= 1 && dim <= 3, "assemble_prediction() is not implemented for this dimension.");
+            if constexpr (dim == 1)
+            {
+                assemble_prediction_1D(A);
+            }
+            else if constexpr (dim == 2)
+            {
+                assemble_prediction_2D(A);
+            }
+            else if constexpr (dim == 3)
+            {
+                assemble_prediction_3D(A);
+            }
+        }
+
+        void assemble_prediction_1D(Mat& A) const
+        {
+            std::array<double, 3> pred{{1./8, 0, -1./8}};
+            for_each_prediction_ghost(_mesh, [&](auto& ghost)
+            {
+                for (unsigned int field_i = 0; field_i < field_size; ++field_i)
+                {
+                    PetscInt ghost_index = data_index(ghost, field_i);
+                    MatSetValue(A, ghost_index, ghost_index, 1, INSERT_VALUES);
+
+                    auto ii = ghost.indices(0);
+                    int sign_i = (ii & 1) ? -1 : 1;
+
+                    auto parent_index = data_index(static_cast<PetscInt>(_mesh.get_index(ghost.level - 1, ii/2)), field_i);
+                    auto parent_left  = data_index(parent_index - 1, field_i);
+                    auto parent_right = data_index(parent_index + 1, field_i);
+                    MatSetValue(A, ghost_index, parent_index,                -1, INSERT_VALUES);
+                    MatSetValue(A, ghost_index, parent_left,  -sign_i * pred[0], INSERT_VALUES);
+                    MatSetValue(A, ghost_index, parent_right, -sign_i * pred[2], INSERT_VALUES);
+                }
+            });
+
+            /*using mesh_id_t = typename Mesh::mesh_id_t;
+
+            auto min_level = mesh[mesh_id_t::cells].min_level();
+            auto max_level = mesh[mesh_id_t::cells].max_level();
+            for(std::size_t level=min_level+1; level<=max_level; ++level)
+            {
+                auto set = intersection(mesh[mesh_id_t::cells_and_ghosts][level],
+                                                mesh[mesh_id_t::cells][level-1])
+                        .on(level);
+
+                //std::array<double, 3> pred{{1./8, 0, -1./8}};
+                set([&](const auto& i, const auto&)
+                {
+                    for(int ii=i.start; ii<i.end; ++ii)
+                    {
+                        auto i_cell = static_cast<int>(mesh.get_index(level, ii));
+                        MatSetValue(A, i_cell, i_cell, 1., INSERT_VALUES);
+
+                        int sign_i = (ii & 1)? -1: 1;
+
+                        for(int is = -1; is<2; ++is)
+                        {
+                            auto i1 = static_cast<int>(mesh.get_index(level - 1, (ii>>1) + is));
+                            double v = -sign_i*pred[static_cast<unsigned int>(is + 1)];
+                            MatSetValue(A, i_cell, i1, v, INSERT_VALUES);
+                        }
+
+                        auto i0 = static_cast<int>(mesh.get_index(level - 1, (ii>>1)));
+                        MatSetValue(A, i_cell, i0, -1., INSERT_VALUES);
+                    }
+                });
+            }*/
+        }
+
+        void assemble_prediction_2D(Mat& A) const
+        {
+            std::array<double, 3> pred{{1./8, 0, -1./8}};
+            for_each_prediction_ghost(_mesh, [&](auto& ghost)
+            {
+                for (unsigned int field_i = 0; field_i < field_size; ++field_i)
+                {
+                    PetscInt ghost_index = static_cast<PetscInt>(data_index(ghost, field_i));
+                    MatSetValue(A, ghost_index, ghost_index, 1, INSERT_VALUES);
+
+                    auto ii = ghost.indices(0);
+                    auto  j = ghost.indices(1);
+                    int sign_i = (ii & 1) ? -1 : 1;
+                    int sign_j =  (j & 1) ? -1 : 1;
+
+                    auto parent              = data_index(static_cast<PetscInt>(_mesh.get_index(ghost.level - 1, ii/2, j/2    )), field_i);
+                    auto parent_bottom       = data_index(static_cast<PetscInt>(_mesh.get_index(ghost.level - 1, ii/2, j/2 - 1)), field_i);
+                    auto parent_top          = data_index(static_cast<PetscInt>(_mesh.get_index(ghost.level - 1, ii/2, j/2 + 1)), field_i);
+                    auto parent_left         = data_index(parent - 1       , field_i);
+                    auto parent_right        = data_index(parent + 1       , field_i);
+                    auto parent_bottom_left  = data_index(parent_bottom - 1, field_i);
+                    auto parent_bottom_right = data_index(parent_bottom + 1, field_i);
+                    auto parent_top_left     = data_index(parent_top - 1   , field_i);
+                    auto parent_top_right    = data_index(parent_top + 1   , field_i);
+
+                    MatSetValue(A, ghost_index, parent             ,                                  -1, INSERT_VALUES);
+                    MatSetValue(A, ghost_index, parent_bottom      ,                   -sign_j * pred[0], INSERT_VALUES); //        sign_j * -1/8
+                    MatSetValue(A, ghost_index, parent_top         ,                   -sign_j * pred[2], INSERT_VALUES); //        sign_j *  1/8
+                    MatSetValue(A, ghost_index, parent_left        ,                   -sign_i * pred[0], INSERT_VALUES); // sign_i        * -1/8
+                    MatSetValue(A, ghost_index, parent_right       ,                   -sign_i * pred[2], INSERT_VALUES); // sign_i        *  1/8
+                    MatSetValue(A, ghost_index, parent_bottom_left , sign_i * sign_j * pred[0] * pred[0], INSERT_VALUES); // sign_i*sign_j *  1/64
+                    MatSetValue(A, ghost_index, parent_bottom_right, sign_i * sign_j * pred[2] * pred[0], INSERT_VALUES); // sign_i*sign_j * -1/64
+                    MatSetValue(A, ghost_index, parent_top_left    , sign_i * sign_j * pred[0] * pred[2], INSERT_VALUES); // sign_i*sign_j * -1/64
+                    MatSetValue(A, ghost_index, parent_top_right   , sign_i * sign_j * pred[2] * pred[2], INSERT_VALUES); // sign_i*sign_j *  1/64
+                }
+            });
+
+            /*using mesh_id_t = typename Mesh::mesh_id_t;
+
+            auto min_level = _mesh[mesh_id_t::cells].min_level();
+            auto max_level = _mesh[mesh_id_t::cells].max_level();
+            for(std::size_t level=min_level+1; level<=max_level; ++level)
+            {
+                auto set = intersection(_mesh[mesh_id_t::cells_and_ghosts][level],
+                                        _mesh[mesh_id_t::cells][level-1])
+                        .on(level);
+
+                std::array<double, 3> pred{{1./8, 0, -1./8}};
+                set([&](const auto& i, const auto& index)
+                {
+                    auto j = index[0];
+                    int sign_j = (j & 1)? -1: 1;
+
+                    for(int ii=i.start; ii<i.end; ++ii)
+                    {
+                        auto i_cell = static_cast<PetscInt>(_mesh.get_index(level, ii, j));
+                        MatSetValue(A, i_cell, i_cell, 1, INSERT_VALUES);
+
+                        int sign_i = (ii & 1)? -1: 1;
+
+                        for(int is = -1; is<2; ++is)
+                        {
+                            // is = -1 --> parent_bottom --> -sign_j*pred[0]
+                            // is =  0 --> parent        --> -sign_j*pred[1]
+                            // is =  1 --> parent_top    --> -sign_j*pred[2]
+                            auto i1 = static_cast<PetscInt>(_mesh.get_index(level - 1, (ii>>1), (j>>1) + is));
+                            MatSetValue(A, i_cell, i1, -sign_j*pred[static_cast<unsigned int>(is + 1)], INSERT_VALUES);
+
+                            // is = -1 --> parent_left  --> -sign_i*pred[0]
+                            // is =  0 --> parent       --> -sign_i*pred[1]
+                            // is =  1 --> parent_right --> -sign_i*pred[2]
+                            i1 = static_cast<PetscInt>(_mesh.get_index(level - 1, (ii>>1) + is, (j>>1)));
+                            MatSetValue(A, i_cell, i1, -sign_i*pred[static_cast<unsigned int>(is + 1)], INSERT_VALUES);
+                        }
+
+                        auto parent_bottom_left  = static_cast<PetscInt>(_mesh.get_index(level - 1, (ii>>1) - 1, (j>>1) - 1));
+                        auto parent_bottom_right = static_cast<PetscInt>(_mesh.get_index(level - 1, (ii>>1) + 1, (j>>1) - 1));
+                        auto parent_top_left     = static_cast<PetscInt>(_mesh.get_index(level - 1, (ii>>1) - 1, (j>>1) + 1));
+                        auto parent_top_right    = static_cast<PetscInt>(_mesh.get_index(level - 1, (ii>>1) + 1, (j>>1) + 1));
+
+                        MatSetValue(A, i_cell, parent_bottom_left , sign_i*sign_j*pred[0]*pred[0], INSERT_VALUES);
+                        MatSetValue(A, i_cell, parent_bottom_right, sign_i*sign_j*pred[2]*pred[0], INSERT_VALUES);
+                        MatSetValue(A, i_cell, parent_top_left    , sign_i*sign_j*pred[0]*pred[2], INSERT_VALUES);
+                        MatSetValue(A, i_cell, parent_top_right   , sign_i*sign_j*pred[2]*pred[2], INSERT_VALUES);
+
+                        auto i0 = static_cast<PetscInt>(_mesh.get_index(level - 1, (ii>>1), (j>>1)));
+                        MatSetValue(A, i_cell, i0, -1, INSERT_VALUES);
+                    }
+                });
+            }*/
+        }
+
+        void assemble_prediction_3D(Mat&) const
+        {
+            for_each_prediction_ghost(_mesh, [&](auto&)
+            {
+                assert(false && "assemble_prediction_3D() not implemented.");
+            });
         }
 
 
@@ -553,146 +721,3 @@ namespace samurai { namespace petsc
     };
 
 }} // end namespace
-
-
-
-//-----------------------------//
-//     Assemble prediction     //
-//          (order 1)          //
-//-----------------------------//
-
-// 1D
-
-template<class Mesh>
-void assemble_prediction_impl(std::integral_constant<std::size_t, 1>, Mat& A, Mesh& mesh)
-{
-    std::array<double, 3> pred{{1./8, 0, -1./8}};
-
-    for_each_prediction_ghost(mesh, [&](auto& ghost)
-    {
-        auto ghost_index = static_cast<PetscInt>(ghost.index);
-        MatSetValue(A, ghost_index, ghost_index, 1, INSERT_VALUES);
-
-        auto ii = ghost.indices(0);
-        int sign_i = (ii & 1) ? -1 : 1;
-
-        auto parent_index = static_cast<PetscInt>(mesh.get_index(ghost.level - 1, ii/2));
-        auto parent_left  = parent_index - 1;
-        auto parent_right = parent_index + 1;
-        MatSetValue(A, ghost_index, parent_index,                -1, INSERT_VALUES);
-        MatSetValue(A, ghost_index, parent_left,  -sign_i * pred[0], INSERT_VALUES);
-        MatSetValue(A, ghost_index, parent_right, -sign_i * pred[2], INSERT_VALUES);
-    });
-
-    /*using mesh_id_t = typename Mesh::mesh_id_t;
-
-    auto min_level = mesh[mesh_id_t::cells].min_level();
-    auto max_level = mesh[mesh_id_t::cells].max_level();
-    for(std::size_t level=min_level+1; level<=max_level; ++level)
-    {
-        auto set = intersection(mesh[mesh_id_t::cells_and_ghosts][level],
-                                        mesh[mesh_id_t::cells][level-1])
-                .on(level);
-
-        //std::array<double, 3> pred{{1./8, 0, -1./8}};
-        set([&](const auto& i, const auto&)
-        {
-            for(int ii=i.start; ii<i.end; ++ii)
-            {
-                auto i_cell = static_cast<int>(mesh.get_index(level, ii));
-                MatSetValue(A, i_cell, i_cell, 1., INSERT_VALUES);
-
-                int sign_i = (ii & 1)? -1: 1;
-
-                for(int is = -1; is<2; ++is)
-                {
-                    auto i1 = static_cast<int>(mesh.get_index(level - 1, (ii>>1) + is));
-                    double v = -sign_i*pred[static_cast<unsigned int>(is + 1)];
-                    MatSetValue(A, i_cell, i1, v, INSERT_VALUES);
-                }
-
-                auto i0 = static_cast<int>(mesh.get_index(level - 1, (ii>>1)));
-                MatSetValue(A, i_cell, i0, -1., INSERT_VALUES);
-            }
-        });
-    }*/
-}
-
-
-// 2D
-
-template<class Mesh>
-void assemble_prediction_impl(std::integral_constant<std::size_t, 2>, Mat& A, Mesh& mesh)
-{
-    using mesh_id_t = typename Mesh::mesh_id_t;
-
-    auto min_level = mesh[mesh_id_t::cells].min_level();
-    auto max_level = mesh[mesh_id_t::cells].max_level();
-    for(std::size_t level=min_level+1; level<=max_level; ++level)
-    {
-        auto set = intersection(mesh[mesh_id_t::cells_and_ghosts][level],
-                                mesh[mesh_id_t::cells][level-1])
-                .on(level);
-
-        std::array<double, 3> pred{{1./8, 0, -1./8}};
-        set([&](const auto& i, const auto& index)
-        {
-            auto j = index[0];
-            int sign_j = (j & 1)? -1: 1;
-
-            for(int ii=i.start; ii<i.end; ++ii)
-            {
-                auto i_cell = static_cast<PetscInt>(mesh.get_index(level, ii, j));
-                MatSetValue(A, i_cell, i_cell, 1, INSERT_VALUES);
-
-                int sign_i = (ii & 1)? -1: 1;
-
-                for(int is = -1; is<2; ++is)
-                {
-                    auto i1 = static_cast<PetscInt>(mesh.get_index(level - 1, (ii>>1), (j>>1) + is));
-                    MatSetValue(A, i_cell, i1, -sign_j*pred[static_cast<unsigned int>(is + 1)], INSERT_VALUES);
-
-                    i1 = static_cast<PetscInt>(mesh.get_index(level - 1, (ii>>1) + is, (j>>1)));
-                    MatSetValue(A, i_cell, i1, -sign_i*pred[static_cast<unsigned int>(is + 1)], INSERT_VALUES);
-                }
-
-                auto i1 = static_cast<PetscInt>(mesh.get_index(level - 1, (ii>>1) - 1, (j>>1) - 1));
-                auto i2 = static_cast<PetscInt>(mesh.get_index(level - 1, (ii>>1) + 1, (j>>1) - 1));
-                auto i3 = static_cast<PetscInt>(mesh.get_index(level - 1, (ii>>1) - 1, (j>>1) + 1));
-                auto i4 = static_cast<PetscInt>(mesh.get_index(level - 1, (ii>>1) + 1, (j>>1) + 1));
-
-                MatSetValue(A, i_cell, i1, sign_i*sign_j*pred[0]*pred[0], INSERT_VALUES);
-                MatSetValue(A, i_cell, i2, sign_i*sign_j*pred[2]*pred[0], INSERT_VALUES);
-                MatSetValue(A, i_cell, i3, sign_i*sign_j*pred[0]*pred[2], INSERT_VALUES);
-                MatSetValue(A, i_cell, i4, sign_i*sign_j*pred[2]*pred[2], INSERT_VALUES);
-
-                auto i0 = static_cast<PetscInt>(mesh.get_index(level - 1, (ii>>1), (j>>1)));
-                MatSetValue(A, i_cell, i0, -1, INSERT_VALUES);
-            }
-        });
-    }
-}
-
-
-// 3D
-
-template<class Mesh>
-void assemble_prediction_impl(std::integral_constant<std::size_t, 3>, Mat& /*A*/, Mesh& mesh)
-{
-    using mesh_id_t = typename Mesh::mesh_id_t;
-
-    auto min_level = mesh[mesh_id_t::cells].min_level();
-    auto max_level = mesh[mesh_id_t::cells].max_level();
-    for(std::size_t level=min_level+1; level<=max_level; ++level)
-    {
-        auto set = intersection(mesh[mesh_id_t::cells_and_ghosts][level],
-                                mesh[mesh_id_t::cells][level-1])
-                .on(level);
-
-        //std::array<double, 3> pred{{1./8, 0, -1./8}};
-        set([&](const auto&, const auto&)
-        {
-            assert(false && "non implemented");
-        });
-    }
-}
