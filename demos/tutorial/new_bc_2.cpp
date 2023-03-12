1cad3287e75472ed09f379e92285bc7ade6f0b41
Loic Gouarin <loic.gouarin@gmail.com>
2023-03-12 10:08:38 +0100

    ...

--- demos/tutorial/new_bc_2.cpp
+++ demos/tutorial/new_bc_2.cpp
@@ -3,37 +3,48 @@
 #include <vector>
 #include <memory>
 #include <functional>
+#include <type_traits>
+
+#include <xtensor/xfixed.hpp>
 
 #include <samurai/level_cell_array.hpp>
 #include <samurai/subset/subset_op.hpp>
+#include <samurai/field.hpp>
 
 // BcValue
 /////////////////////////////////////////////////////////////////////////////////////////
 template<std::size_t dim, class T, std::size_t size>
 struct BcValue
 {
+    using value_t = xt::xtensor_fixed<T, xt::xshape<size>>;
+    using coords_t = xt::xtensor_fixed<T, xt::xshape<dim>>;
+
     BcValue(){}
     virtual ~BcValue() = default;
 
-    virtual std::array<T, size>& apply(const std::array<T, dim>&) = 0;
+    virtual value_t& apply(const coords_t&) = 0;
     virtual BcValue* clone() const = 0;
 };
 
 template<std::size_t dim, class T, std::size_t size>
 struct ConstantBc: public BcValue<dim, T, size>
 {
+    using base_t = BcValue<dim, T, size>;
+    using value_t = typename base_t::value_t;
+    using coords_t = typename base_t::coords_t;
+
     ConstantBc(T v)
     {
         m_v.fill(v);
     }
 
-    ConstantBc(const std::array<T, size>& v)
+    ConstantBc(const value_t& v)
     : m_v(v)
     {}
 
     virtual ~ConstantBc() = default;
 
-    inline std::array<T, size>& apply(const std::array<T, dim>&) override
+    inline value_t& apply(const coords_t&) override
     {
         std::cout << "constant" << std::endl;
         return m_v;
@@ -44,12 +55,16 @@ struct ConstantBc: public BcValue<dim, T, size>
         return new ConstantBc(m_v);
     }
 
-    std::array<T, size> m_v;
+    value_t m_v;
 };
 
 template<std::size_t dim, class T, std::size_t size>
 struct FunctionBc: public BcValue<dim, T, size>
 {
+    using base_t = BcValue<dim, T, size>;
+    using value_t = typename base_t::value_t;
+    using coords_t = typename base_t::coords_t;
+
     template <class Func>
     FunctionBc(Func &&f)
     :m_func(std::forward<Func>(f))
@@ -57,7 +72,7 @@ struct FunctionBc: public BcValue<dim, T, size>
 
     virtual ~FunctionBc() = default;
 
-    inline std::array<T, size>& apply(const std::array<T, dim>& coords) override
+    inline value_t& apply(const coords_t& coords) override
     {
         std::cout << "function" << std::endl;
 
@@ -70,8 +85,8 @@ struct FunctionBc: public BcValue<dim, T, size>
         return new FunctionBc(m_func);
     }
 
-    std::function<std::array<T, size>(const std::array<double, dim>&)> m_func;
-    std::array<T, size> m_v;
+    std::function<value_t (const coords_t&)> m_func;
+    value_t m_v;
 };
 
 // BcRegion
@@ -171,6 +186,12 @@ auto make_region(Func&& func)
     return std::make_unique<CoordsRegion<dim>>(std::forward<Func>(func));
 }
 
+template<std::size_t dim>
+auto make_region(Everywhere<dim>)
+{
+    return std::make_unique<Everywhere<dim>>();
+}
+
 // Bc
 /////////////////////////////////////////////////////////////////////////////////////////
 template<std::size_t dim, class T, std::size_t size>
@@ -227,19 +248,31 @@ struct Robin: public Bc<dim, T, size>
     using Bc<dim, T, size>::Bc;
 };
 
-template<std::size_t dim, class T, std::size_t size>
-struct Field
+template<class mesh_t_, class value_t = double, std::size_t size_ = 1, bool SOA=false>
+struct MyField: public samurai::Field<mesh_t_, value_t, size_, SOA>
 {
-    Field(){}
+    using base_type = samurai::Field<mesh_t_, value_t, size_, SOA>;
+    static constexpr std::size_t dim = base_type::dim;
+
+    using base_type::base_type;
 
     template<class Bc_derived>
-    void attach(const Bc_derived& bc)
+    auto attach(const Bc_derived& bc)
     {
         //p_bc.push_back(std::unique_ptr<Bc_derived>(bc.clone()));
         p_bc.push_back(std::make_unique<Bc_derived>(bc));
+        return *p_bc.back().get();
     }
-    std::vector<std::unique_ptr<Bc<dim, T, size>>> p_bc;
+
+    std::vector<std::unique_ptr<Bc<dim, value_t, size_>>> p_bc;
 };
+
+template <class value_t, std::size_t size, bool SOA=false, class mesh_t>
+auto make_field(std::string name, mesh_t& mesh)
+{
+    using field_t = MyField<mesh_t, value_t, size, SOA>;
+    return field_t(name, mesh);
+}
 
 template<class Field, std::size_t dim=Field::dim, class T = typename Field::type, std::size_t size=Field::size>
 void apply(const Dirichlet<dim, T, size>& dirichlet, Field& field)
@@ -255,54 +288,99 @@ void apply(const Neumann<dim, T, size>& neumann, Field& field)
     std::cout << neumann.p_bcvalue->apply({1, 2})[0] << std::endl;
 }
 
+template<template<std::size_t, class, std::size_t> class bc_type, class Field, class... T>
+auto make_bc(Field& field, T... v)
+{
+    using value_t = typename Field::value_type;
+    constexpr std::size_t dim = Field::dim;
+    constexpr std::size_t size = Field::size;
+
+    static_assert(std::is_same_v<typename Field::value_type, std::common_type_t<T...>>, "The constant value type must be the same as the field value_type");
+    static_assert(Field::size == sizeof...(T), "The number of constant values should be equal to the number of element in the field");
+
+    return field.attach(bc_type<dim, value_t, size>(ConstantBc<dim, value_t, size>(v...)));
+}
+
+template<template<std::size_t, class, std::size_t> class bc_type, class Field, class Func>
+auto make_bc_func(Field& field, Func&& func)
+{
+    using value_t = typename Field::value_type;
+    constexpr std::size_t dim = Field::dim;
+    constexpr std::size_t size = Field::size;
+
+    // static_assert(std::is_same_v<typename Field::value_type, std::common_type_t<T...>>, "The constant value type must be the same as the field value_type");
+    // static_assert(Field::size == sizeof...(T), "The number of constant values should be equal to the number of element in the field");
+
+    return field.attach(bc_type<dim, value_t, size>(FunctionBc<dim, value_t, size>(std::forward<Func>(func))));
+}
+
 int main()
 {
     constexpr std::size_t dim = 2;
     samurai::Box<double, dim> box = {{0, 0}, {1, 1}};
     samurai::LevelCellArray<dim> lca = {2, box};
 
-    Field<2, double, 3> f;
-    Dirichlet<2, double, 3> dirichlet(ConstantBc<2, double, 3>{4});
-    dirichlet.on([](auto& coords)
+    auto u = ::make_field<double, 1>("u", lca);
+
+    auto bc = make_bc<Dirichlet>(u, 1.);
+    bc.on([](auto& coords)
     {
         return (coords[0] >= .25 && coords[0] <= .75);
     });
 
-    f.attach(dirichlet);
+    std::cout << bc.get_lca(u.mesh()) << std::endl;
 
-    // Neumann<2, double, 3> neumann(ConstantBc<2, double, 3>{4});
-    Neumann<2, double, 3> neumann(FunctionBc<2, double, 3>([](auto& coords) -> std::array<double, 3>
+    bc.on(Everywhere<dim>());
+    std::cout << bc.get_lca(u.mesh()) << std::endl;
+
+    std::cout << u.p_bc.back().get()->get_lca(u.mesh()) << std::endl;
+
+    make_bc_func<Dirichlet>(u, [](auto& coords)
     {
-        return {coords[0], 2*coords[1], 3*coords[0]*coords[1]};
-    }));
-    neumann.on(samurai::difference(lca, samurai::translate(lca, xt::xtensor_fixed<int, xt::xshape<1>>{1})));
-    f.attach(neumann);
+        return 1;
+        // return xt::xtensor_fixed<double, xt::xshape<1>>(1);
+    });
 
-    std::cout << "lca" << std::endl;
-    std::cout << lca << std::endl;
-    std::cout << "dirichlet" << std::endl;
-    std::cout << dirichlet.get_lca(lca) << std::endl;
-    std::cout << "neumann" << std::endl;
-    std::cout << neumann.get_lca(lca) << std::endl;
+    // Field<2, double, 3> f;
+    // Dirichlet<2, double, 3> dirichlet(ConstantBc<2, double, 3>{4});
+    // dirichlet.on([](auto& coords)
+    // {
+    //     return (coords[0] >= .25 && coords[0] <= .75);
+    // });
 
-    Robin<2, double, 3> robin(ConstantBc<2, double, 3>{4});
-    f.attach(robin);
+    // // Neumann<2, double, 3> neumann(ConstantBc<2, double, 3>{4});
+    // Neumann<2, double, 3> neumann(FunctionBc<2, double, 3>([](auto& coords) -> std::array<double, 3>
+    // {
+    //     return {coords[0], 2*coords[1], 3*coords[0]*coords[1]};
+    // }));
+    // neumann.on(samurai::difference(lca, samurai::translate(lca, xt::xtensor_fixed<int, xt::xshape<1>>{1})));
+    // f.attach(neumann);
 
-    for(auto& p: f.p_bc)
-    {
-        if (dynamic_cast<Dirichlet<2, double, 3>*>(p.get()))
-        {
-            apply(*dynamic_cast<Dirichlet<2, double, 3>*>(p.get()), f);
-        }
-        else if (dynamic_cast<Neumann<2, double, 3>*>(p.get()))
-        {
-            apply(*dynamic_cast<Neumann<2, double, 3>*>(p.get()), f);
-        }
-        else
-        {
-            std::cout << "not known" << std::endl;
-        }
-    }
+    // std::cout << "lca" << std::endl;
+    // std::cout << lca << std::endl;
+    // std::cout << "dirichlet" << std::endl;
+    // std::cout << dirichlet.get_lca(lca) << std::endl;
+    // std::cout << "neumann" << std::endl;
+    // std::cout << neumann.get_lca(lca) << std::endl;
+
+    // Robin<2, double, 3> robin(ConstantBc<2, double, 3>{4});
+    // f.attach(robin);
+
+    // for(auto& p: f.p_bc)
+    // {
+    //     if (dynamic_cast<Dirichlet<2, double, 3>*>(p.get()))
+    //     {
+    //         apply(*dynamic_cast<Dirichlet<2, double, 3>*>(p.get()), f);
+    //     }
+    //     else if (dynamic_cast<Neumann<2, double, 3>*>(p.get()))
+    //     {
+    //         apply(*dynamic_cast<Neumann<2, double, 3>*>(p.get()), f);
+    //     }
+    //     else
+    //     {
+    //         std::cout << "not known" << std::endl;
+    //     }
+    // }
 
     return 0;
 }
