063f42189061eba1c1d82147ce7b7a9c1ed694f3
Loic Gouarin <loic.gouarin@gmail.com>
2023-11-13 09:50:45 +0100

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -216,10 +216,10 @@ todo_include_todos = True
 
 # -- Breathe configuration for readthedocs -----------------------------------
 
-# import subprocess, os
+import subprocess, os
 
-# read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
-# if read_the_docs_build:
+if read_the_docs_build:
 
-#     subprocess.call('cd ..; doxygen', shell=True)
+    subprocess.call('cd ..; doxygen', shell=True)
