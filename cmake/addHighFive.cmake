4940c761cba76fdf61f80abc6ecbe99b40bdb1ba
Loic Gouarin <loic.gouarin@gmail.com>
2023-12-21 15:18:22 +0100

    remove highfive dep in pixi

--- cmake/addHighFive.cmake
+++ cmake/addHighFive.cmake
@@ -1,11 +1,11 @@
 include(FetchContent)
-FetchContent_Declare(highfive
+FetchContent_Declare(HighFive
     GIT_REPOSITORY      https://github.com/BlueBrain/HighFive
     GIT_TAG             v2.8.0)
 FetchContent_GetProperties(highfive)
-if(NOT highfive_POPULATED)
-    FetchContent_Populate(highfive)
+if(NOT HighFive_POPULATED)
+    FetchContent_Populate(HighFive)
     set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE BOOL "")
-    add_subdirectory(${highfive_SOURCE_DIR} ${highfive_BINARY_DIR} EXCLUDE_FROM_ALL)
+    add_subdirectory(${HighFive_SOURCE_DIR} ${HighFive_BINARY_DIR} EXCLUDE_FROM_ALL)
     unset(CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
 endif()
