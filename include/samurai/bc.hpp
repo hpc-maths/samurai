77953e7daa8bb784d29a1e52bb846ad845c8ccbb
Pierre Matalon <pierre.matalon@gmail.com>
2024-02-28 10:06:35 +0100

    Test CI with ACTIONS_RUNNER_DEBUG: true

--- include/samurai/bc.hpp
+++ include/samurai/bc.hpp
@@ -14,7 +14,6 @@
 #include <xtensor/xnoalias.hpp>
 #include <xtensor/xview.hpp>
 
-// #include "boundary.hpp"
 #include "samurai/cell.hpp"
 #include "samurai_config.hpp"
 #include "static_algorithm.hpp"
