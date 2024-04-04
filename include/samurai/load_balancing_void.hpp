aa8996993b2c780d5fe274e6f80894209ab46f26
L. Strafella <loic.strafella@polytechnique.edu>
2024-04-04 14:15:20 +0200

    add void load balancing class for demo

---
+++ include/samurai/load_balancing_void.hpp
@@ -0,0 +1,36 @@
+/**
+ * Empty class, used for test to compare with and without load balancing. 
+ *  
+*/
+
+#pragma once
+
+#include <map>
+#include "load_balancing.hpp"
+
+template<int dim>
+class Void_LoadBalancer: public samurai::LoadBalancer<Void_LoadBalancer<dim>> {
+
+    private:
+        int _ndomains;
+        int _rank;
+
+    public:
+
+        Void_LoadBalancer() {
+#ifdef SAMURAI_WITH_MPI
+            boost::mpi::communicator world;
+            _ndomains = world.size();
+            _rank     = world.rank();
+#else
+            _ndomains = 1;
+            _rank     = 0;
+#endif
+        }
+
+        inline std::string getName() const { return "Void_LB"; }
+
+        template<class Mesh_t>
+        void load_balance_impl( Mesh_t & mesh ){ }
+
+};
