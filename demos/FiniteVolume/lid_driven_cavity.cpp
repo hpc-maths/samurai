168e98ee630bc4c87c1572033dd0b73b075fd3bc
Loic Gouarin <loic.gouarin@gmail.com>
2024-04-13 08:29:29 +0200

    modify header file

--- demos/FiniteVolume/lid_driven_cavity.cpp
+++ demos/FiniteVolume/lid_driven_cavity.cpp
@@ -1,6 +1,5 @@
-// Copyright 2021 SAMURAI TEAM. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2018-2024 the samurai's authors
+// SPDX-License-Identifier:  BSD-3-Clause
 
 #include <CLI/CLI.hpp>
 
