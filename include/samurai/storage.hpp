9f6bfd30fc67b43a881b4be2b7d4c0f370f3d90d
Loic Gouarin <loic.gouarin@gmail.com>
2024-05-18 17:36:16 +0200

    add storage list and enable automatic configuration

---
+++ include/samurai/storage.hpp
@@ -0,0 +1,11 @@
+// Copyright 2018-2024 the samurai's authors
+// SPDX-License-Identifier:  BSD-3-Clause
+
+#pragma once
+
+#ifdef SAMURAI_FIELD_CONTAINER_EIGEN3
+#include "storage/eigen.hpp"
+#endif
+#ifdef SAMURAI_FIELD_CONTAINER_XTENSOR
+#include "storage/xtensor.hpp"
+#endif
