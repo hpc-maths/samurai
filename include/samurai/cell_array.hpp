83d71d5155d7d1ab7529a62cdd4732a9b4a61e3c
Loic Gouarin <loic.gouarin@gmail.com>
2024-05-31 14:22:37 +0200

    Add functions to compute the number of intervals in CellArray

--- include/samurai/cell_array.hpp
+++ include/samurai/cell_array.hpp
@@ -119,6 +119,9 @@ namespace samurai
         std::size_t nb_cells() const;
         std::size_t nb_cells(std::size_t level) const;
 
+        std::size_t nb_intervals(std::size_t dim) const;
+        std::size_t nb_intervals(std::size_t dim, std::size_t level) const;
+
         std::size_t max_level() const;
         std::size_t min_level() const;
 
@@ -373,6 +376,38 @@ namespace samurai
     inline std::size_t CellArray<dim_, TInterval, max_size_>::nb_cells(std::size_t level) const
     {
         return m_cells[level].nb_cells();
+    }
+
+    /**
+     * Return the number of intervals for a given dimension over the levels.
+     *
+     * @param d The dimension where to compute the number of intervals
+     * @return The number of intervals for the given dimension
+     */
+    template <std::size_t dim_, class TInterval, std::size_t max_size_>
+    inline std::size_t CellArray<dim_, TInterval, max_size_>::nb_intervals(std::size_t d) const
+    {
+        assert(d < dim);
+        std::size_t size = 0;
+        for (std::size_t level = 0; level <= max_size; ++level)
+        {
+            size += m_cells[level].nb_intervals(d);
+        }
+        return size;
+    }
+
+    /**
+     * Return the number of intervals for a given dimension and a given level.
+     *
+     * @param d The dimension where to compute the number of intervals
+     * @param level The level where to compute the number of intervals
+     * @return The number of intervals for the given dimension
+     */
+    template <std::size_t dim_, class TInterval, std::size_t max_size_>
+    inline std::size_t CellArray<dim_, TInterval, max_size_>::nb_intervals(std::size_t d, std::size_t level) const
+    {
+        assert(d < dim);
+        return m_cells[level].nb_intervals(d);
     }
 
     /**
