6ff21702dd5a797046cd5a51182a4f70ae5bbd29
Uilian Ries <uilianries@gmail.com>
2024-06-03 10:59:09 +0200

    Update Conan recipe to install demo and tests
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,43 @@
-from conan import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
 
 class SamuraiConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "build_demos": [True, False],
+        "build_tests": [True, False],
+    }
+    default_options = {
+        "build_demos": False,
+        "build_tests": False,
+    }
     requires = [
-        "xtensor/0.24.6",
-        "highfive/2.7.1",
+        "xtensor/0.24.7",
+        "highfive/2.9.0",
         "hdf5/1.14.3",
         "pugixml/1.14",
-        "cli11/3.2.0",
-        "cxxopts/3.0.0",
-        "fmt/10.1.1",
-        "rapidcheck/cci.20220514",
+        "cli11/2.4.2",
+        "cxxopts/3.2.0",
+        "fmt/10.2.1",
+        "rapidcheck/cci.20230815"
     ]
     generators = ["CMakeDeps", "CMakeToolchain"]
     default_options = {
-        "hdf5:shared": False,
-        "highfive:with_boost": False,
-        "highfive:with_opencv": False,
-        "highfive:with_eigen": False,
-        "rapidcheck:enable_gtest": True,
+        "hdf5/*:shared": False,
+        "highfive/*:with_boost": False,
+        "highfive/*:with_opencv": False,
+        "highfive/*:with_eigen": False,
+        "rapidcheck/*:enable_gtest": True,
     }
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        if self.options.build_demos:
+            self.requires("cgal/5.6.1")
+        if self.options.build_tests:
+            self.requires("gtest/1.14.0")
 
     def build(self):
         cmake = CMake(self)
