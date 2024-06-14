5621cad6ac49a8d6ad3091376485e7ea1e0bdfd6
L. Strafella <loic.strafella@polytechnique.edu>
2024-06-14 10:07:55 +0200

    fix subdomain ghosts update for multi component field

--- include/samurai/algorithm/update.hpp
+++ include/samurai/algorithm/update.hpp
@@ -207,9 +207,9 @@ namespace samurai
                     [&](const auto& i, const auto& index)
                     {
                         std::copy(to_recv.begin() + count,
-                                  to_recv.begin() + count + static_cast<ptrdiff_t>(i.size()),
+                                  to_recv.begin() + count + static_cast<ptrdiff_t>(i.size() * field.size),
                                   field(level, i, index).begin());
-                        count += static_cast<ptrdiff_t>(i.size());
+                        count += static_cast<ptrdiff_t>(i.size() * field.size);
                     });
             }
         }
