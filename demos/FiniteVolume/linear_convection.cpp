7c935dc12a740a5987f0ec3cd1a9504d9ff65c73
L. Strafella <loic.strafella@polytechnique.edu>
2024-07-25 10:46:00 +0200

    fix new name for LB

--- demos/FiniteVolume/linear_convection.cpp
+++ demos/FiniteVolume/linear_convection.cpp
@@ -161,7 +161,8 @@ int main(int argc, char* argv[])
     auto conv = samurai::make_convection_weno5<decltype(u)>(velocity);
 
     // SFC_LoadBalancer_interval<dim, Morton> balancer;
-    Load_balancing::Life balancer;
+    // Load_balancing::Life balancer;
+    Load_balancing::GlobalCriteria balancer;
     // Void_LoadBalancer<dim> balancer;
     // Diffusion_LoadBalancer_cell<dim> balancer;
     // Diffusion_LoadBalancer_interval<dim> balancer;
@@ -203,10 +204,6 @@ int main(int argc, char* argv[])
             samurai::times::timers.start("tloop.lb:"+balancer.getName());
             balancer.load_balance(mesh, u);
             samurai::times::timers.stop("tloop.lb:"+balancer.getName());
-
-            // std::string _stats = fmt::format("statistics_process_{}", world.rank());
-            // samurai::Statistics s(_stats);
-            // s("statistics", mesh);
 
         }
         
