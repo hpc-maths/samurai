7b9a3d77e42d983a4d3443d26a32b593077abe57
sbstndbs <sebastiendubois4@gmail.com>
2025-02-11 23:26:27 +0100

    Add benchmarks

--- benchmark/benchmark_cellarray.cpp
+++ benchmark/benchmark_cellarray.cpp
@@ -64,6 +64,40 @@ void CELLARRAY_min_level(benchmark::State& state){
 }
 
 
+template <unsigned int dim>
+void CELLARRAY_begin(benchmark::State& state){
+        samurai::CellList<dim> cl ;
+        cl[state.range(0)][{}].add_interval({0,1}) ;
+        samurai::CellArray<dim> ca (cl) ;
+        for (auto _ : state){
+                auto begin = ca.begin() ;
+                benchmark::DoNotOptimize(begin);
+        }
+}
+
+template <unsigned int dim>
+void CELLARRAY_end(benchmark::State& state){
+        samurai::CellList<dim> cl ;
+        cl[state.range(0)][{}].add_interval({0,1}) ;
+        samurai::CellArray<dim> ca (cl) ;
+        for (auto _ : state){
+                auto end = ca.end() ;
+                benchmark::DoNotOptimize(end);
+        }
+}
+
+
+template <unsigned int dim>
+void CELLARRAY_rbegin(benchmark::State& state){
+        samurai::CellList<dim> cl ;
+        cl[state.range(0)][{}].add_interval({0,1}) ;
+        samurai::CellArray<dim> ca (cl) ;
+        for (auto _ : state){
+                auto rbegin = ca.rbegin() ;
+                benchmark::DoNotOptimize(rbegin);
+        }
+}
+
 
 
 static void CELLARRAY_CellList2CellArray_2D(benchmark::State& state)
@@ -122,6 +156,37 @@ static void CELLARRAY_CellList2CellArray_3D(benchmark::State& state)
 
 BENCHMARK(CELLARRAY_CellList2CellArray_3D)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
 
+static void CELLARRAY_equal_2D(benchmark::State& state)
+{
+    constexpr std::size_t dim = 2;
+
+    std::size_t min_level = 1;
+    std::size_t max_level = 12;
+
+    samurai::CellList<dim> cl;
+    samurai::CellArray<dim> ca;
+    samurai::CellArray<dim> ca2;
+
+    for (std::size_t s = 0; s < state.range(0); ++s)
+    {
+        auto level = std::experimental::randint(min_level, max_level);
+        auto x     = std::experimental::randint(0, (100 << level) - 1);
+        auto y     = std::experimental::randint(0, (100 << level) - 1);
+
+        cl[level][{y}].add_point(x);
+    }
+    ca = {cl} ; 
+    ca2 = {cl} ;
+
+    for (auto _ : state)
+    {
+        auto equal = ca == ca2 ;
+        benchmark::DoNotOptimize(equal) ;	
+    }
+}
+
+BENCHMARK(CELLARRAY_equal_2D)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
+
 
 
 BENCHMARK_TEMPLATE(CELLARRAY_default,1, 12);
@@ -135,4 +200,18 @@ BENCHMARK_TEMPLATE(CELLARRAY_cl_ca_multi,3)->RangeMultiplier(2)->Range(1 << 1, 1
 BENCHMARK_TEMPLATE(CELLARRAY_min_level,1)->DenseRange(0,15);
 BENCHMARK_TEMPLATE(CELLARRAY_min_level,2)->DenseRange(0,15);
 BENCHMARK_TEMPLATE(CELLARRAY_min_level,3)->DenseRange(0,15);
+
+BENCHMARK_TEMPLATE(CELLARRAY_begin,1)->DenseRange(0,15);
+BENCHMARK_TEMPLATE(CELLARRAY_begin,2)->DenseRange(0,15);
+BENCHMARK_TEMPLATE(CELLARRAY_begin,3)->DenseRange(0,15);
+
+BENCHMARK_TEMPLATE(CELLARRAY_rbegin,1)->DenseRange(0,15);
+BENCHMARK_TEMPLATE(CELLARRAY_rbegin,2)->DenseRange(0,15);
+BENCHMARK_TEMPLATE(CELLARRAY_rbegin,3)->DenseRange(0,15);
+
+
+BENCHMARK_TEMPLATE(CELLARRAY_end,1)->DenseRange(0,15);
+BENCHMARK_TEMPLATE(CELLARRAY_end,2)->DenseRange(0,15);
+BENCHMARK_TEMPLATE(CELLARRAY_end,3)->DenseRange(0,15);
+
 
