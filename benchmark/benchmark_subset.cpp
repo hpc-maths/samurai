a11a576fadb3b4e7491b1005b8a696da86a14b0c
sbstndb/sbstndbs <sebastiendubois4@gmail.com>
2025-03-03 17:28:51 +0100

    Update benchmark

--- benchmark/benchmark_subset.cpp
+++ benchmark/benchmark_subset.cpp
@@ -17,6 +17,9 @@
 
 
 
+// Observation : Cela prend environ 10ns par intervalle
+// si on compare 2 intervalles de taillen, cela prendra environ 2n * 10ns
+
 ///////////////////////////////////////////////////////////////////
 // utils
 
@@ -64,6 +67,60 @@ void SUBSET_difference_same_interval_different_level(benchmark::State& state){
 		}
         }
 }
+
+
+template <unsigned int dim, unsigned int delta_level >
+void SUBSET_difference_different_interval_different_level(benchmark::State& state){
+        samurai::CellList<dim> cl1, cl2 ;
+        for (int64_t i = 0 ; i < state.range(0); i++){
+                int index = static_cast<int>(i) ;
+                cl1[0][{}].add_interval({2*index, 2*index+1});
+                cl2[delta_level][{}].add_interval({pow(2, delta_level+1)*index + pow(2, delta_level), pow(2, delta_level+2)*index});		
+        }
+        samurai::CellArray<dim> ca1(cl1);
+        samurai::CellArray<dim> ca2(cl2);
+        for (auto _ : state){
+                if constexpr(delta_level != 0){
+                        auto subset = samurai::difference(ca1[0], samurai::projection(ca2[delta_level], 0));
+                        subset([](const auto&, const auto&) {}); // evaluation avec lambda vide
+                        benchmark::DoNotOptimize(subset);
+                }
+                else if constexpr (delta_level == 0){
+                        auto subset = samurai::difference(ca1[0], ca2[0]);
+                        subset([](const auto&, const auto&) {}); // evaluation avec lambda vide         
+                        benchmark::DoNotOptimize(subset);
+                }
+        }
+}
+
+
+
+
+template <unsigned int dim, unsigned int delta_level >
+void SUBSET_difference_n1_interval_different_level(benchmark::State& state){
+        samurai::CellList<dim> cl1, cl2 ;
+        for (int64_t i = 0 ; i < state.range(0); i++){
+                int index = static_cast<int>(i) ;
+                cl1[0][{}].add_interval({2*index, 2*index+1});
+        }
+        cl2[delta_level][{}].add_interval({static_cast<int>(0), static_cast<int>(pow(2, delta_level))});
+	
+        samurai::CellArray<dim> ca1(cl1);
+        samurai::CellArray<dim> ca2(cl2);
+        for (auto _ : state){
+                if constexpr(delta_level != 0){
+                        auto subset = samurai::difference(ca1[0], samurai::projection(ca2[delta_level], 0));
+                        subset([](const auto&, const auto&) {}); // evaluation avec lambda vide
+                        benchmark::DoNotOptimize(subset);
+                }
+                else if constexpr (delta_level == 0){
+                        auto subset = samurai::difference(ca1[0], ca2[0]);
+                        subset([](const auto&, const auto&) {}); // evaluation avec lambda vide         
+                        benchmark::DoNotOptimize(subset);
+                }
+        }
+}
+
 
 
 
@@ -234,6 +291,12 @@ BENCHMARK_TEMPLATE(SUBSET_difference_same_interval_different_level,3, 2)->RangeM
 BENCHMARK_TEMPLATE(SUBSET_difference_same_interval_different_level,1, 10)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
 BENCHMARK_TEMPLATE(SUBSET_difference_same_interval_different_level,2, 10)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
 BENCHMARK_TEMPLATE(SUBSET_difference_same_interval_different_level,3, 10)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
+
+
+BENCHMARK_TEMPLATE(SUBSET_difference_n1_interval_different_level,1, 0)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
+BENCHMARK_TEMPLATE(SUBSET_difference_n1_interval_different_level,2, 0)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
+BENCHMARK_TEMPLATE(SUBSET_difference_n1_interval_different_level,3, 0)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
+
 
 
 BENCHMARK_TEMPLATE(SUBSET_double_difference_same_interval,1, 0)->RangeMultiplier(2)->Range(1 << 1, 1 << 10);
