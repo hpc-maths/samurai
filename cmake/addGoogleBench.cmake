c98aaac4104a53019e7d8f6d6aaeaab5b12aba15
Loic Gouarin <loic.gouarin@gmail.com>
2025-04-07 16:14:14 +0200

    update googletest and benchmark version

--- cmake/addGoogleBench.cmake
+++ cmake/addGoogleBench.cmake
@@ -1,11 +1,8 @@
 include(FetchContent)
 FetchContent_Declare(googlebench
     GIT_REPOSITORY      https://github.com/google/benchmark.git
-    GIT_TAG             v1.8.4)
+    GIT_TAG             v1.9.2)
 FetchContent_GetProperties(googlebench)
 if(NOT googlebench_POPULATED)
-    FetchContent_Populate(googlebench)
-    set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE BOOL "")
-    add_subdirectory(${googlebench_SOURCE_DIR} ${googlebench_BINARY_DIR} EXCLUDE_FROM_ALL)
-    unset(CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
+    FetchContent_MakeAvailable(googlebench)
 endif()
