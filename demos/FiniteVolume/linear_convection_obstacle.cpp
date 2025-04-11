d930dd482f240381d414990902340aaed352cb62
Pierre Matalon <pierre.matalon@gmail.com>
2025-04-11 09:09:26 +0200

    Fix rebase

--- demos/FiniteVolume/linear_convection_obstacle.cpp
+++ demos/FiniteVolume/linear_convection_obstacle.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier:  BSD-3-Clause
 
 #include <samurai/domain_builder.hpp>
-#include <samurai/hdf5.hpp>
+#include <samurai/io/hdf5.hpp>
 #include <samurai/mr/adapt.hpp>
 #include <samurai/mr/mesh.hpp>
 #include <samurai/samurai.hpp>
