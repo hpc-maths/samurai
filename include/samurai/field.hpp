2ccda98c799b39f07f6d71f959529ebb24fee53f
sbstndb/sbstndbs <sebastiendubois4@gmail.com>
2025-04-22 14:23:42 +0200

    fix: move parenthesis

--- include/samurai/field.hpp
+++ include/samurai/field.hpp
@@ -1189,14 +1189,14 @@ namespace samurai
     {
         using mesh_id_t = typename mesh_t::mesh_id_t;
         // add comment : the CI is buggy ahahaha
-        return iterator(this, this->mesh()[mesh_id_t::cells].cbegin());
+        return iterator(this, this->mesh()[mesh_id_t::cells]).cbegin();
     }
 
     template <class mesh_t, class value_t>
     inline auto ScalarField<mesh_t, value_t>::end() -> iterator
     {
         using mesh_id_t = typename mesh_t::mesh_id_t;
-        return iterator(this, this->mesh()[mesh_id_t::cells]).cend();
+        return iterator(this, this->mesh()[mesh_id_t::cells].cend());
     }
 
     template <class mesh_t, class value_t>
