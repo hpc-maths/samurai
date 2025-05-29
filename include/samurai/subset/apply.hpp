3fdf7d80827fadeb881a49e994ae0507be524dfb
Loic Gouarin <loic.gouarin@gmail.com>
2025-05-29 07:20:40 +0200

    add exist function

--- include/samurai/subset/apply.hpp
+++ include/samurai/subset/apply.hpp
@@ -10,8 +10,8 @@ namespace samurai
 {
     namespace detail
     {
-        template <std::size_t dim, class Set, class Func, class Container>
-        void apply_impl(Set&& global_set, Func&& func, Container& index)
+        template <std::size_t dim, bool check = false, class Set, class Func, class Container>
+        bool apply_impl(Set&& global_set, Func&& func, Container& index)
         {
             auto set            = global_set.template get_local_set<dim>(global_set.level(), index);
             auto start_and_stop = global_set.template get_start_and_stop_function<dim>();
@@ -23,36 +23,64 @@ namespace samurai
                     for (auto i = interval.start; i < interval.end; ++i)
                     {
                         index[dim - 2] = i;
-                        apply_impl<dim - 1>(std::forward<Set>(global_set), std::forward<Func>(func), index);
+                        if (apply_impl<dim - 1, check>(std::forward<Set>(global_set), std::forward<Func>(func), index))
+                        {
+                            return true;
+                        }
                     }
+                    return false;
                 };
-                apply(set, start_and_stop, func_int);
+                return apply<check>(set, start_and_stop, func_int);
             }
             else
             {
                 auto func_int = [&](const auto& interval)
                 {
-                    func(interval, index);
+                    return func(interval, index);
                 };
-                apply(set, start_and_stop, func_int);
+                return apply<check>(set, start_and_stop, func_int);
             }
         }
     }
 
     template <class Set, class Func>
-    void apply(Set&& global_set, Func&& func)
+    void apply(Set&& global_set, Func&& user_func)
     {
         constexpr std::size_t dim = std::decay_t<Set>::dim;
         xt::xtensor_fixed<int, xt::xshape<dim - 1>> index;
+
+        auto func = [&](const auto& interval, const auto& index)
+        {
+            user_func(interval, index);
+            return false;
+        };
+
         if (global_set.exist())
         {
-            detail::apply_impl<dim>(std::forward<Set>(global_set), std::forward<Func>(func), index);
+            detail::apply_impl<dim>(std::forward<Set>(global_set), func, index);
         }
     }
 
-    template <class Set, class StartEnd, class Func>
+    template <class Set>
+    bool exist(Set&& global_set)
+    {
+        constexpr std::size_t dim = std::decay_t<Set>::dim;
+        xt::xtensor_fixed<int, xt::xshape<dim - 1>> index;
+
+        auto func = [](const auto&, const auto&)
+        {
+            return true;
+        };
+
+        if (global_set.exist())
+        {
+            detail::apply_impl<dim, true>(std::forward<Set>(global_set), func, index);
+        }
+    }
+
+    template <bool check = false, class Set, class StartEnd, class Func>
         requires IsSetOp<Set> || IsIntervalListVisitor<Set>
-    void apply(Set&& set, StartEnd&& start_and_stop, Func&& func)
+    bool apply(Set&& set, StartEnd&& start_and_stop, Func&& func)
     {
         using interval_t = typename std::decay_t<Set>::interval_t;
         using value_t    = typename interval_t::value_t;
@@ -78,11 +106,15 @@ namespace samurai
 
                 auto true_result = set.shift() >= 0 ? result >> static_cast<std::size_t>(set.shift())
                                                     : result << -static_cast<std::size_t>(set.shift());
-                func(true_result);
+                if (func(true_result))
+                {
+                    return true;
+                }
             }
 
             set.next(scan, std::forward<StartEnd>(start_and_stop));
             scan = set.min();
         }
+        return false;
     }
 }
