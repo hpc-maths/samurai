06562b291a3700afc04e66dd19ddddfbcd1ef4a4
sbstndb/sbstndbs <sebastiendubois4@gmail.com>
2025-07-12 00:19:37 +0200

    feat: load balancing

---
+++ demos/FiniteVolume/advection_2d_load_balancing.cpp
@@ -0,0 +1,304 @@
+// Copyright 2018-2025 the samurai's authors
+// SPDX-License-Identifier:  BSD-3-Clause
+
+#include <array>
+
+#include <xtensor/xfixed.hpp>
+
+#include <samurai/algorithm.hpp>
+#include <samurai/bc.hpp>
+#include <samurai/field.hpp>
+#include <samurai/io/hdf5.hpp>
+#include <samurai/io/restart.hpp>
+#include <samurai/mr/adapt.hpp>
+#include <samurai/mr/mesh.hpp>
+#include <samurai/samurai.hpp>
+#include <samurai/stencil_field.hpp>
+#include <samurai/subset/node.hpp>
+#include <samurai/load_balancing.hpp>
+#include <samurai/load_balancing_diffusion.hpp>
+
+
+#include <filesystem>
+namespace fs = std::filesystem;
+
+template <class Field>
+void init(Field& u)
+{
+    auto& mesh = u.mesh();
+    u.resize();
+
+    samurai::for_each_cell(
+        mesh,
+        [&](auto& cell)
+        {
+            auto center           = cell.center();
+            const double radius   = .2;
+            const double x_center = 0.3;
+            const double y_center = 0.3;
+            if (((center[0] - x_center) * (center[0] - x_center) + (center[1] - y_center) * (center[1] - y_center)) <= radius * radius)
+            {
+                u[cell] = 1;
+            }
+            else
+            {
+                u[cell] = 0;
+            }
+        });
+}
+
+template <class Field>
+void flux_correction(double dt, const std::array<double, 2>& a, const Field& u, Field& unp1)
+{
+    using mesh_t              = typename Field::mesh_t;
+    using mesh_id_t           = typename mesh_t::mesh_id_t;
+    using interval_t          = typename mesh_t::interval_t;
+    constexpr std::size_t dim = Field::dim;
+
+    auto mesh = u.mesh();
+
+    for (std::size_t level = mesh.min_level(); level < mesh.max_level(); ++level)
+    {
+        xt::xtensor_fixed<int, xt::xshape<dim>> stencil;
+
+        stencil = {
+            {-1, 0}
+        };
+
+        auto subset_right = samurai::intersection(samurai::translate(mesh[mesh_id_t::cells][level + 1], stencil),
+                                                  mesh[mesh_id_t::cells][level])
+                                .on(level);
+
+        subset_right(
+            [&](const auto& i, const auto& index)
+            {
+                auto j          = index[0];
+                const double dx = mesh.cell_length(level);
+
+                unp1(level, i, j) = unp1(level, i, j)
+                                  + dt / dx
+                                        * (samurai::upwind_op<dim, interval_t>(level, i, j).right_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i + 1, 2 * j).right_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i + 1, 2 * j + 1).right_flux(a, u));
+            });
+
+        stencil = {
+            {1, 0}
+        };
+
+        auto subset_left = samurai::intersection(samurai::translate(mesh[mesh_id_t::cells][level + 1], stencil),
+                                                 mesh[mesh_id_t::cells][level])
+                               .on(level);
+
+        subset_left(
+            [&](const auto& i, const auto& index)
+            {
+                auto j          = index[0];
+                const double dx = mesh.cell_length(level);
+
+                unp1(level, i, j) = unp1(level, i, j)
+                                  - dt / dx
+                                        * (samurai::upwind_op<dim, interval_t>(level, i, j).left_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i, 2 * j).left_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i, 2 * j + 1).left_flux(a, u));
+            });
+
+        stencil = {
+            {0, -1}
+        };
+
+        auto subset_up = samurai::intersection(samurai::translate(mesh[mesh_id_t::cells][level + 1], stencil), mesh[mesh_id_t::cells][level])
+                             .on(level);
+
+        subset_up(
+            [&](const auto& i, const auto& index)
+            {
+                auto j          = index[0];
+                const double dx = mesh.cell_length(level);
+
+                unp1(level, i, j) = unp1(level, i, j)
+                                  + dt / dx
+                                        * (samurai::upwind_op<dim, interval_t>(level, i, j).up_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i, 2 * j + 1).up_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i + 1, 2 * j + 1).up_flux(a, u));
+            });
+
+        stencil = {
+            {0, 1}
+        };
+
+        auto subset_down = samurai::intersection(samurai::translate(mesh[mesh_id_t::cells][level + 1], stencil),
+                                                 mesh[mesh_id_t::cells][level])
+                               .on(level);
+
+        subset_down(
+            [&](const auto& i, const auto& index)
+            {
+                auto j          = index[0];
+                const double dx = mesh.cell_length(level);
+
+                unp1(level, i, j) = unp1(level, i, j)
+                                  - dt / dx
+                                        * (samurai::upwind_op<dim, interval_t>(level, i, j).down_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i, 2 * j).down_flux(a, u)
+                                           - .5 * samurai::upwind_op<dim, interval_t>(level + 1, 2 * i + 1, 2 * j).down_flux(a, u));
+            });
+    }
+}
+
+template <class Field>
+void save(const fs::path& path, const std::string& filename, const Field& u, const std::string& suffix = "")
+{
+    auto mesh   = u.mesh();
+    auto level_ = samurai::make_scalar_field<std::size_t>("level", mesh);
+
+    if (!fs::exists(path))
+    {
+        fs::create_directory(path);
+    }
+
+    samurai::for_each_cell(mesh,
+                           [&](const auto& cell)
+                           {
+                               level_[cell] = cell.level;
+                           });
+#ifdef SAMURAI_WITH_MPI
+    mpi::communicator world;
+    samurai::save(path, fmt::format("{}_size_{}{}", filename, world.size(), suffix), mesh, u, level_);
+#else
+    samurai::save(path, fmt::format("{}{}", filename, suffix), mesh, u, level_);
+    samurai::dump(path, fmt::format("{}_restart{}", filename, suffix), mesh, u);
+#endif
+}
+
+int main(int argc, char* argv[])
+{
+    auto& app = samurai::initialize("Finite volume example for the advection equation in 2d using multiresolution", argc, argv);
+
+    constexpr std::size_t dim = 2;
+    using Config              = samurai::MRConfig<dim>;
+
+    // Simulation parameters
+    xt::xtensor_fixed<double, xt::xshape<dim>> min_corner = {0., 0.};
+    xt::xtensor_fixed<double, xt::xshape<dim>> max_corner = {1., 1.};
+    std::array<double, dim> a{
+        {1, 1}
+    };
+    double Tf  = .1;
+    double cfl = 0.5;
+    double t   = 0.;
+    std::string restart_file;
+
+    // Multiresolution parameters
+    std::size_t min_level = 4;
+    std::size_t max_level = 10;
+    double mr_epsilon     = 2.e-4; // Threshold used by multiresolution
+    double mr_regularity  = 1.;    // Regularity guess for multiresolution
+    bool correction       = false;
+
+    // Output parameters
+    fs::path path        = fs::current_path();
+    std::string filename = "FV_advection_2d";
+    std::size_t nfiles   = 1;
+    std::size_t nt_loadbalance = 1;
+
+    app.add_option("--min-corner", min_corner, "The min corner of the box")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--max-corner", max_corner, "The max corner of the box")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--velocity", a, "The velocity of the advection equation")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--cfl", cfl, "The CFL")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--Ti", t, "Initial time")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--Tf", Tf, "Final time")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--restart-file", restart_file, "Restart file")->capture_default_str()->group("Simulation parameters");
+    app.add_option("--min-level", min_level, "Minimum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--max-level", max_level, "Maximum level of the multiresolution")->capture_default_str()->group("Multiresolution");
+    app.add_option("--mr-eps", mr_epsilon, "The epsilon used by the multiresolution to adapt the mesh")
+        ->capture_default_str()
+        ->group("Multiresolution");
+    app.add_option("--mr-reg",
+                   mr_regularity,
+                   "The regularity criteria used by the multiresolution to "
+                   "adapt the mesh")
+        ->capture_default_str()
+        ->group("Multiresolution");
+    app.add_option("--with-correction", correction, "Apply flux correction at the interface of two refinement levels")
+        ->capture_default_str()
+        ->group("Multiresolution");
+    app.add_option("--path", path, "Output path")->capture_default_str()->group("Output");
+    app.add_option("--filename", filename, "File name prefix")->capture_default_str()->group("Output");
+    app.add_option("--nfiles", nfiles, "Number of output files")->capture_default_str()->group("Output");
+#ifdef SAMURAI_WITH_MPI
+    app.add_option("--nt-loadbalance", nt_loadbalance, "load balance each nt steps")->capture_default_str()->group("Multiresolution");
+#endif    
+    SAMURAI_PARSE(argc, argv);
+
+    const samurai::Box<double, dim> box(min_corner, max_corner);
+    samurai::MRMesh<Config> mesh;
+    auto u = samurai::make_scalar_field<double>("u", mesh);
+
+    if (restart_file.empty())
+    {
+        mesh = {box, min_level, max_level};
+        init(u);
+    }
+    else
+    {
+        samurai::load(restart_file, mesh, u);
+    }
+    samurai::make_bc<samurai::Dirichlet<1>>(u, 0.);
+
+    double dt            = cfl * mesh.cell_length(max_level);
+    const double dt_save = Tf / static_cast<double>(nfiles);
+
+    auto unp1 = samurai::make_scalar_field<double>("unp1", mesh);
+
+    auto MRadaptation = samurai::make_MRAdapt(u);
+    MRadaptation(mr_epsilon, mr_regularity);
+    save(path, filename, u, "_init");
+
+    std::size_t nsave = 1;
+    std::size_t nt    = 0;
+
+#ifdef SAMURAI_WITH_MPI
+	samurai::DiffusionLoadBalancer balancer; 
+#endif
+
+    while (t != Tf)
+    {
+#ifdef SAMURAI_WITH_MPI
+	if (((nt % nt_loadbalance == 0) && nt > 1) || nt == 1)
+	{
+		auto weights = samurai::Weight::uniform(mesh);
+		balancer.load_balance(mesh, weights, u);
+	}
+#endif
+
+        MRadaptation(mr_epsilon, mr_regularity);
+
+        t += dt;
+        if (t > Tf)
+        {
+            dt += Tf - t;
+            t = Tf;
+        }
+
+        std::cout << fmt::format("iteration {}: t = {}, dt = {}", nt++, t, dt) << std::endl;
+
+        samurai::update_ghost_mr(u);
+        unp1.resize();
+        unp1 = u - dt * samurai::upwind(a, u);
+        if (correction)
+        {
+            flux_correction(dt, a, u, unp1);
+        }
+
+        std::swap(u.array(), unp1.array());
+
+        if (t >= static_cast<double>(nsave + 1) * dt_save || t == Tf)
+        {
+            const std::string suffix = (nfiles != 1) ? fmt::format("_ite_{}", nsave++) : "";
+            save(path, filename, u, suffix);
+        }
+    }
+    samurai::finalize();
+    return 0;
+}
---
+++ include/samurai/load_balancing_diffusion.hpp
@@ -0,0 +1,168 @@
+#include "field.hpp"
+#include "load_balancing.hpp"
+#include "timers.hpp"
+
+#include <algorithm>
+#include <iterator>
+
+#ifdef SAMURAI_WITH_MPI
+namespace samurai
+{
+
+    class DiffusionLoadBalancer : public samurai::LoadBalancer<DiffusionLoadBalancer>
+    {
+      private:
+
+        template <samurai::BalanceElement_t elem, class Mesh_t, class Field_t>
+        std::vector<double> compute_fluxes(Mesh_t& mesh, const Field_t& weight, int niterations)
+        {
+            samurai::times::timers.start("load_balancing_flux_computation");
+            
+            using mpi_subdomain_t = typename Mesh_t::mpi_subdomain_t;
+            boost::mpi::communicator world;
+            std::vector<mpi_subdomain_t>& neighbourhood = mesh.mpi_neighbourhood();
+            size_t n_neighbours                         = neighbourhood.size();
+
+            // Load of current process
+            double my_load = samurai::Weight::compute_load<elem>(mesh, weight);
+            // Fluxes between processes
+            std::vector<double> fluxes(n_neighbours, 0.);
+            // Load of each process (all processes not only neighbours)
+            std::vector<double> loads;
+            int iteration_count = 0;
+            while (iteration_count < niterations)
+            {
+                boost::mpi::all_gather(world, my_load, loads);
+
+                // Compute updated my_load for current process based on its neighbourhood
+                double my_load_new = my_load;
+                bool all_fluxes_zero = true;
+                for (std::size_t neighbour_idx = 0; neighbour_idx < n_neighbours; ++neighbour_idx)
+                {
+                    std::size_t neighbour_rank = static_cast<std::size_t>(neighbourhood[neighbour_idx].rank);
+                    double neighbour_load         = loads[neighbour_rank];
+                    double diff_load = neighbour_load - my_load_new;
+
+                    // If transferLoad < 0 -> need to send data, if transferLoad > 0 need to receive data
+                    // TODO : Use diffusion factor 1/(deg+1) for stability
+                    double transfertLoad = 0.5* diff_load;
+
+                    // Accumulate total flux on current edge
+                    fluxes[neighbour_idx] += transfertLoad;
+
+                    // Mark if a non-zero transfer was performed
+                    if (transfertLoad != 0)
+                    {
+                        all_fluxes_zero = false;
+                    }
+
+                    // Update intermediate local load before processing next neighbour
+                    my_load_new += transfertLoad;
+                }
+                
+                // Update reference load for next iteration
+                my_load = my_load_new;
+
+                // Check if all processes have reached convergence
+                bool global_convergence = boost::mpi::all_reduce(world, all_fluxes_zero, std::logical_and<bool>());
+
+                // If all processes have zero fluxes, state will no longer change
+                if (global_convergence)
+                {
+                    std::cout << "Process " << world.rank() << " : Global convergence reached at iteration " << iteration_count << std::endl;
+                    break;
+                }
+                
+                iteration_count++;
+            }
+            
+            samurai::times::timers.stop("load_balancing_flux_computation");
+            
+            return fluxes;
+        }
+
+      public:
+
+        DiffusionLoadBalancer() = default;
+
+        template <class Mesh_t, class Weight_t>
+        auto load_balance_impl(Mesh_t& mesh, const Weight_t& weight)
+        {
+            using mesh_id_t = typename Mesh_t::mesh_id_t;
+            boost::mpi::communicator world;
+
+            auto flags = samurai::make_scalar_field<int>("diffusion_flag", mesh);
+            flags.fill(world.rank());
+
+            // Compute fluxes in terms of load to transfer/receive
+            std::vector<double> fluxes = compute_fluxes<samurai::BalanceElement_t::CELL>(mesh, weight, 50);
+
+            using cell_t = typename Mesh_t::cell_t;
+            std::vector<cell_t> cells;
+            samurai::for_each_cell(mesh[mesh_id_t::cells],
+                                   [&](auto cell)
+                                   {
+                                       cells.emplace_back(cell);
+                                   });
+
+            if (cells.empty())
+            {
+                return flags;
+            }
+
+            // Sort cells from "top" to "bottom", then from "left" to "right"
+            std::sort(cells.begin(),
+                      cells.end(),
+                      [](const cell_t& a, const cell_t& b)
+                      {
+                          auto center_a = a.center();
+                          auto center_b = b.center();
+                          if (center_a(1) != center_b(1))
+                          {
+                              return center_a(1) > center_b(1); // First, cells with highest y coordinate
+                          }
+                          return center_a(0) < center_b(0); // Then, cells with lowest x coordinate
+                      });
+
+            auto& neighbourhood = mesh.mpi_neighbourhood();
+            
+            std::size_t top_index = 0;                   
+            std::size_t bottom_index = cells.size() - 1;
+
+            for (std::size_t i = 0; i < neighbourhood.size(); ++i)
+            {
+                double flux = fluxes[i];
+                auto neighbour_rank = neighbourhood[i].rank;
+
+                if (flux < 0) // We must send cells
+                {
+                    double weight_to_send = -flux;
+                    double accumulated_weight = 0;
+
+                    // Send from the "top" to higher ranks, and from the "bottom" to lower ranks
+                    if (neighbour_rank > world.rank())
+                    {
+                        while (top_index <= bottom_index && accumulated_weight < weight_to_send)
+                        {
+                            accumulated_weight += weight[cells[top_index]];
+                            flags[cells[top_index]] = neighbour_rank;
+                            top_index++;
+                        }
+                    }
+                    else
+                    {
+                        while (bottom_index >= top_index && accumulated_weight < weight_to_send)
+                        {
+                            accumulated_weight += weight[cells[bottom_index]];
+                            flags[cells[bottom_index]] = neighbour_rank;
+                            if (bottom_index == 0) break; // Éviter l'underflow
+                            bottom_index--;
+                        }
+                    }
+                }
+            }
+            return flags;
+        }
+    };
+}
+#endif
