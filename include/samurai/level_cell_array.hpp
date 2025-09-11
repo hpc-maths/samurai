f2544be24f9723ba4fd6637bbaf354155a6e1388
Ubuntu <sebastiendubois4@gmail.com>
2025-09-11 14:22:41 +0000

    fix: remove warnings at compile time

--- include/samurai/level_cell_array.hpp
+++ include/samurai/level_cell_array.hpp
@@ -9,6 +9,7 @@
 #include <type_traits>
 #include <vector>
 
+
 #ifdef SAMURAI_WITH_MPI
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/vector.hpp>
@@ -45,7 +46,11 @@ namespace samurai
         explicit LevelCellArray_reverse_iterator(iterator&& it)
             : base_type(std::move(it))
         {
-        }
+}
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
 
         const coord_type index() const
         {
@@ -494,6 +499,7 @@ namespace samurai
         typename iterator::offset_type_iterator offset_index;
         typename iterator::iterator_container current_index;
         typename iterator::coord_type index;
+        index.fill(0);
 
         for (std::size_t d = 0; d < dim; ++d)
         {
@@ -514,6 +520,7 @@ namespace samurai
         typename iterator::offset_type_iterator offset_index;
         typename iterator::iterator_container current_index;
         typename iterator::coord_type index;
+        index.fill(0);
 
         for (std::size_t d = 0; d < dim; ++d)
         {
@@ -536,6 +543,7 @@ namespace samurai
         typename const_iterator::offset_type_iterator offset_index;
         typename const_iterator::iterator_container current_index;
         typename const_iterator::coord_type index;
+        index.fill(0);
 
         for (std::size_t d = 0; d < dim; ++d)
         {
@@ -558,6 +566,7 @@ namespace samurai
         typename const_iterator::offset_type_iterator offset_index;
         typename const_iterator::iterator_container current_index;
         typename const_iterator::coord_type index;
+        index.fill(0);
 
         for (std::size_t d = 0; d < dim; ++d)
         {
