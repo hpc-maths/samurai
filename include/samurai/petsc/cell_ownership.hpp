b5e97295bc14a07f922b2d65042fbe43702855a7
Pierre Matalon <pierre.matalon@gmail.com>
2025-12-05 08:00:22 +0100

    Fix codacy

--- include/samurai/petsc/cell_ownership.hpp
+++ include/samurai/petsc/cell_ownership.hpp
@@ -13,11 +13,11 @@ namespace samurai
             std::size_t n_owned_cells = 0;
 
             // Owner rank of each cell in the local mesh
-            std::vector<int> owner_rank;
+            std::vector<int> owner_rank; // cppcheck-suppress unusedStructMember
             // Renumbering of the cells: first all the owned cells, then all the ghosts.
             // This is used to split the ordering of the unknowns (first all the owned unknowns, then all the ghosts).
             // Note that the cell numbers start at index 0, and the ghost numbers also start at index 0!
-            std::vector<int> cell_indices;
+            std::vector<int> cell_indices; // cppcheck-suppress unusedStructMember
         };
     }
 }
